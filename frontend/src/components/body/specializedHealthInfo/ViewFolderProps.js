5b92b94b7d3601d25ed12ff3ebf051ab2d3eb7b1
Siana Rizwan <sianarizwan@gmail.com>
2021-07-31 20:09:00 +0600

    Update ViewFolderProps.js

--- frontend/src/components/body/specializedHealthInfo/ViewFolderProps.js
+++ frontend/src/components/body/specializedHealthInfo/ViewFolderProps.js
@@ -1,22 +1,14 @@
-import React, { useState, useEffect, useContext, useRef } from "react";
+import React, { useState } from "react";
 import FolderSpecialIcon from "@material-ui/icons/FolderSpecial";
-import {
-  IconButton,
-  CardContent,
-  Button,
-  CardActions,
-  CardHeader,
-} from "@material-ui/core";
+import { IconButton, CardContent, CardActions } from "@material-ui/core";
+import EditNotesModal from "./EditNotesModal";
 import clsx from "clsx";
 import Collapse from "@material-ui/core/Collapse";
 import { makeStyles } from "@material-ui/core/styles";
 import EditIcon from "@material-ui/icons/Edit";
-import axios from "axios";
 import DeleteIcon from "@material-ui/icons/Delete";
-import { useSelector } from "react-redux";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import { Link } from "react-router-dom";
-import CheckBoxIcon from "@material-ui/icons/CheckBox";
 import "../../../static/Styling/spHealthInfo.css";
 
 const useStyles = makeStyles((theme) => ({
@@ -30,37 +22,16 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function ViewFolderProps(props) {
-  const token = useSelector((state) => state.token);
-
-  const [editing, setEditing] = useState(false);
-  const [description, setDesc] = useState("");
   const [expanded, setExpanded] = React.useState(false);
+
+  const [showEditModal, setShowEditModal] = useState(false);
+  const openSpEditModal = () => setShowEditModal(true);
+
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
   const classes = useStyles();
-
-  const updateDesc = async (e, folderId) => {
-    e.preventDefault();
-    console.log("folderId ", folderId);
-    await axios
-      .patch(
-        "http://localhost:5000/api/updateSpecializedHealthInfo/" + folderId,
-        { description },
-        {
-          headers: { Authorization: token },
-        }
-      )
-      .then((response) => {
-        console.log("editfolder", response.data);
-        setEditing(false);
-        props.showSPHealthNotes();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
 
   return (
     <div variant="outlined" className="sp_reminder_card">
@@ -70,7 +41,6 @@ export default function ViewFolderProps(props) {
       </h2>
       <hr></hr>
       <div>Note Date: {props.note.noteDate.substring(0, 10)}</div>
-
       <IconButton
         className={
           (clsx(classes.expand, {
@@ -86,50 +56,25 @@ export default function ViewFolderProps(props) {
           data-toggle="tooltip"
           title="Click To View Details"
         >
-          Description
+          Your Notes
         </h5>
       </IconButton>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>{props.note.description}</CardContent>
       </Collapse>
 
-      {editing ? (
-        <CardContent className="content">
-          {" "}
-          <textarea
-            className="editingBox"
-            value={description}
-            onChange={(e) => setDesc(e.target.value)}
-          >
-            {props.note.description}
-          </textarea>
-        </CardContent>
-      ) : (
-        ""
-      )}
-
       <CardActions className="clrCardAction">
-        {editing ? (
-          <IconButton
-            className="viewBtn"
-            data-toggle="tooltip"
-            title="Update Description"
-            onClick={(e) => updateDesc(e, props.note._id)}
-          >
-            <CheckBoxIcon />
-          </IconButton>
-        ) : (
-          <IconButton
-            className="viewBtn"
-            data-toggle="tooltip"
-            title="Edit Folder"
-            key={props.note.folder}
-            value={props.note.description}
-            onClick={() => setEditing(true)}
-          >
-            <EditIcon />
-          </IconButton>
-        )}
+        <IconButton
+          className="viewBtn"
+          data-toggle="tooltip"
+          title="Edit Folder"
+          key={props.note.folder}
+          value={props.note.description}
+          // onClick={() => setEditing(true)}
+          onClick={openSpEditModal}
+        >
+          <EditIcon />
+        </IconButton>
 
         <IconButton
           component={Link}
@@ -152,6 +97,15 @@ export default function ViewFolderProps(props) {
           <DeleteIcon />
         </IconButton>
       </CardActions>
+      <EditNotesModal
+        key={props.note.folder}
+        showEditModal={showEditModal}
+        setShowEditModal={setShowEditModal}
+        getNote={props.note.description}
+        getID={props.note._id}
+        showSPHealthNotes={props.showSPHealthNotes}
+        getFolder={props.note.folder}
+      />
     </div>
   );
 }
