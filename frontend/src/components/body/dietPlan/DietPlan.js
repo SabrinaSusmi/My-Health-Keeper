40df0d7dfd680c677cd3314819c70bbec29dba59
Siana Rizwan <sianarizwan@gmail.com>
2021-08-02 14:54:23 +0600

    dietPlan table added

--- frontend/src/components/body/dietPlan/DietPlan.js
+++ frontend/src/components/body/dietPlan/DietPlan.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Button, Link } from "@material-ui/core";
 import "../../../static/Styling/dietPlan.css";
+import Table from 'react-bootstrap/Table'
 import { NavLink } from "react-router-dom";
 import DietGoalSetter from "./DietGoalSetter";
 import { useSelector } from "react-redux";
+import IconButton from "@material-ui/core/IconButton";
+import AddCircleOutlineRoundedIcon from "@material-ui/icons/AddCircleOutlineRounded";
 import axios from "axios";
 
 export default function DietPlan() {
@@ -24,6 +27,37 @@ export default function DietPlan() {
       </div>
       {targetVisible ? <DietGoalSetter
        isVisible={() => viewtargetInfo()}  /> : ""}
+       </div>
+       <div className="diet_body">
+         <div className="add_food">
+         <Button className="add_food_btn"> 
+         <IconButton aria-label="add" >
+             <AddCircleOutlineRoundedIcon />
+        </IconButton>
+             {""} Add Food Item</Button>
+         </div>
+         <div className="food_table">
+       <Table  hover size="md">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Food Name</th>
+      <th>Quantity</th>
+      <th>Meal Description</th>
+      <th>Calories Consumed</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Rice</td>
+      <td>2 cups</td>
+      <td>Lunch</td>
+      <td>173</td>
+    </tr>
+  </tbody>
+</Table>
+</div>
        </div>
     </div>
   );
