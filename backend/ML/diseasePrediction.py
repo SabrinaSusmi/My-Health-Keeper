b3ef906fc00a9583a7042467a2872b45ecbcd1fd
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-08-03 23:04:46 +0600

    python file is connected with js file

---
+++ backend/ML/diseasePrediction.py
@@ -0,0 +1,10 @@
+
+import sys
+
+userName = '01771794998'
+password = 'e4855a4a8a'
+recipientNumber = str(sys.argv[1])
+
+
+print('phone ',recipientNumber,str(sys.argv[2]),str(sys.argv[3]),str(sys.argv[4]),str(sys.argv[5]))
+
--- backend/controllers/diseasePrediction/diseasePrediction.controllers.js
+++ backend/controllers/diseasePrediction/diseasePrediction.controllers.js
@@ -1,8 +1,26 @@
+const { spawn } = require("child_process");
 
-const predictDisease = (req,res) => {
- 
-const {s1,s2,s3,s4,s5}=req.body
-console.log(s1,' ',s2,' ',s3,' ',s4,' ',s5)
+const predictDisease = (req, res) => {
+  const { s1, s2, s3, s4, s5 } = req.body;
+  console.log(s1, " ", s2, " ", s3, " ", s4, " ", s5);
+  const pyProg = spawn("python", [
+    "../backend/ML/diseasePrediction.py",
+    s1,
+    s2,
+    s3,
+    s4,
+    s5,
+  ]);
+  pyProg.stdout.on("data", function (data) {
+    console.log("jb", data.toString());
+  });
+  pyProg.stdin.on;
+  pyProg.stderr.on("data", function (data) {
+    console.error(data.toString());
+  });
 
+  pyProg.on("close", function (code) {
+    console.log(code);
+  });
 };
-module.exports = predictDisease ;
+module.exports = predictDisease;
