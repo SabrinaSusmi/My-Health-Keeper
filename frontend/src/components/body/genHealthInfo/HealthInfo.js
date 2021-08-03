90a89aaf18fe16774986c80df2696d08a0e112d7
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-08-04 00:05:05 +0600

    health info modified

--- frontend/src/components/body/genHealthInfo/HealthInfo.js
+++ frontend/src/components/body/genHealthInfo/HealthInfo.js
@@ -1,74 +1,301 @@
-import React, { useRef, useEffect, useCallback } from "react";
-import "react-responsive-modal/styles.css";
-import Modal from "react-bootstrap/Modal";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "../../../static/Styling/healthInfo.css";
+import React from "react";
+import { useState } from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import LocalHospitalRoundedIcon from "@material-ui/icons/LocalHospitalRounded";
+import AddCircleOutlineRoundedIcon from "@material-ui/icons/AddCircleOutlineRounded";
+import InvertColorsIcon from '@material-ui/icons/InvertColors';
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import HeightIcon from "@material-ui/icons/Height";
 import { Button } from "@material-ui/core";
-import { Line } from "react-chartjs-2";
+import WeightModal from "./WeightModal";
+import BpModal from "./BpModal";
+import SugarModal from "./SugarModal";
+import PulseModal from "./PulseModal";
+import { useSelector } from "react-redux";
+import { showErrMsg, showSuccessMsg } from "../../utils/notification/Notification";
+import axios from "axios";
 
-const state = {
-  labels: ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-  datasets: [
-    {
-      label: "Rainfall",
-      fill: false,
-      lineTension: 0.5,
-      backgroundColor: "rgba(75,192,192,1)",
-      borderColor: "rgba(0,0,0,1)",
-      borderWidth: 2,
-      data: [65, 59, 80, 81, 56, 99, 92],
-    },
-  ],
-};
+const weightInitialState = {
+  infoTitleWeight: 'Weight',
+  infoWeight: '',
+  errW: '',
+  successW: ''
+}
 
-const WeightModal = ({ showWeightModal, setShowWeightModal }) => {
-  const closeWeightModal = () => {
-    setShowWeightModal(false);
+const bpInitialState = {
+  infoTitleBp: 'Bp',
+  infoBp: '',
+  errB: '',
+  successB: ''
+}
+
+const pulseInitialState = {
+  infoTitlePulse: 'Pulse',
+  infoPulse: '',
+  errP: '',
+  successP: ''
+}
+
+const sugarInitialState = {
+  infoTitleSugar: 'Sugar',
+  infoSugar: '',
+  errS: '',
+  successS: ''
+}
+
+
+function GeneralHealthInfo() {
+  const token = useSelector((state) => state.token);
+
+  const [showWeightModal, setShowWeightModal] = useState(false);
+  const openWeightModal = () => setShowWeightModal(true);
+
+  const [showBpModal, setShowBpModal] = useState(false);
+  const openBpModal = () => setShowBpModal(true);
+
+  const [showPulseModal, setShowPulseModal] = useState(false);
+  const openPulseModal = () => setShowPulseModal(true);
+ 
+  const [showSugarModal, setShowSugarModal] = useState(false);
+  const openSugarModal = () =>  setShowSugarModal(true);
+
+  const [weight, setWeight] = useState(weightInitialState);
+  const [bp, setBp] = useState(bpInitialState);
+  const [pulse, setPulse] = useState(pulseInitialState);
+  const [sugar, setSugar] = useState(sugarInitialState);
+
+  const { infoTitleWeight, infoWeight, errW, successW } = weight;
+  const { infoTitleBp, infoBp, errB, successB } = bp;
+  const { infoTitlePulse, infoPulse, errP, successP } = pulse;
+  const { infoTitleSugar, infoSugar, errS, successS } = sugar;
+
+
+  const handleChangeWeight = (e) => {
+    const { name, value } = e.target;
+    setWeight({ ...weight, [name]: value, errW: "", successW: "" });
   };
 
-  return (
-    <>
-      <Modal
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        show={showWeightModal}
-        onHide={closeWeightModal}
-      >
-        <Modal.Header>
-          <Modal.Title>
-            <h4>Hello Weight</h4>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h2>Graph</h2>
-          <Line
-            data={state}
-            options={{
-              title: {
-                display: true,
-                text: "Average Rainfall per month",
-                fontSize: 20,
-              },
-              legend: {
-                display: true,
-                position: "right",
-              },
-            }}
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            variant="secondary"
-            onClick={() => {
-              closeWeightModal();
-            }}
-          >
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-};
+  const handleChangeBp = (e) => {
+    const { name, value } = e.target;
+    setBp({ ...bp, [name]: value, errB: "", successB: "" });
+  };
 
-export default WeightModal;
+  const handleChangePulse = (e) => {
+    const { name, value } = e.target;
+    setPulse({ ...pulse, [name]: value, err: "", success: "" });
+  };
+
+  const handleChangeSugar = (e) => {
+    const { name, value } = e.target;
+    setSugar({ ...sugar, [name]: value, err: "", success: "" });
+  };
+
+  const handleSubmitWeight = async (e) => {
+    e.preventDefault();
+    var infoTitle = infoTitleWeight;
+    var info = infoWeight;
+
+    await axios.post("http://localhost:5000/addGenHealth", {
+      infoTitle,
+      info,
+    }, {
+      headers: { Authorization: token },
+    }).then((res)=> {
+      setWeight({ ...weight, errW: "", successW:"Weight added successfully!" });
+    }).catch ((err) => {
+      err.response.data.msg && setWeight({ ...weight, errW: err.response.data.msg, successW:"" });
+    })
+  };
+
+  const handleSubmitBp = async (e) => {
+    e.preventDefault();
+    var infoTitle = infoTitleBp;
+    var info = infoBp;
+
+    await axios.post("http://localhost:5000/addGenHealth", {
+      infoTitle,
+      info,
+    }, {
+      headers: { Authorization: token },
+    }).then((res)=> {
+      setWeight({ ...bp, errB: "", successB:"BP added successfully!" });
+    }).catch ((err) => {
+      err.response.data.msg && setWeight({ ...bp, errB: err.response.data.msg, successB:"" });
+    })
+  };
+
+  const handleSubmitPulse = async (e) => {
+    e.preventDefault();
+    var infoTitle = infoTitlePulse;
+    var info = infoPulse;
+
+    await axios.post("http://localhost:5000/addGenHealth", {
+      infoTitle,
+      info,
+    }, {
+      headers: { Authorization: token },
+    }).then((res)=> {
+      setWeight({ ...pulse, errP: "", successP:"Pulse added successfully!" });
+    }).catch ((err) => {
+      err.response.data.msg && setWeight({ ...pulse, errP: err.response.data.msg, successP:"" });
+    })
+  };
+
+  const handleSubmitSugar = async (e) => {
+    e.preventDefault();
+    var infoTitle = infoTitleSugar;
+    var info = infoSugar;
+
+    await axios.post("http://localhost:5000/addGenHealth", {
+      infoTitle,
+      info,
+    }, {
+      headers: { Authorization: token },
+    }).then((res)=> {
+      setWeight({ ...sugar, errS: "", successS:"Sugar added successfully!" });
+    }).catch ((err) => {
+      err.response.data.msg && setWeight({ ...sugar, errS: err.response.data.msg, successS:"" });
+    })
+  };
+
+
+  return (
+    <div className=" body ">
+      {
+        <div>
+        <Card className="root">
+          <div className="details">
+            <CardContent className="content">
+            {errW && showErrMsg(errW)}
+        {successW && showSuccessMsg(successW)}
+              <Typography component="h5" variant="h5">
+                👣 Weight
+              </Typography>
+              <div className="margin">
+                <Grid container spacing={2} alignItems="flex-end">
+                  <Grid item>
+                    <LocalHospitalRoundedIcon />
+                  </Grid>
+                  <Grid item>
+                    <TextField label="KG" id="infoWeight" name="infoWeight" value={infoWeight} onChange={handleChangeWeight} />
+                  </Grid>
+                  <IconButton aria-label="add" className="controls" onClick= {handleSubmitWeight}>
+                    <AddCircleOutlineRoundedIcon className="playIcon" />
+                  </IconButton>
+                </Grid>
+              </div>
+              <Button className="summary_btn" onClick={openWeightModal}>
+                 Show History</Button>
+            </CardContent>
+          </div>
+          <WeightModal
+        showWeightModal={showWeightModal}
+        setShowWeightModal={setShowWeightModal}
+      />
+        </Card>
+      </div>
+      }
+      {
+        <Card className="root">
+          <div className="details">
+            <CardContent className="content">
+            {errB && showErrMsg(errB)}
+        {successB && showSuccessMsg(successB)}
+              <Typography component="h5" variant="h5">
+                🩸 Blood Pressure
+              </Typography>
+              <div className="margin">
+                <Grid container spacing={2} alignItems="flex-end">
+                  <Grid item>
+                    <InvertColorsIcon />
+                  </Grid>
+                  <Grid item>
+                    <TextField label="bpm" id="infoBp" name="infoBp" value={infoBp} onChange={handleChangeBp} />
+                  </Grid>
+                  <IconButton aria-label="add" className="controls" onClick= {handleSubmitBp} >
+                    <AddCircleOutlineRoundedIcon className="playIcon" />
+                  </IconButton>
+                </Grid>
+              </div>
+              <Button className="summary_btn" onClick={openBpModal}> Show History</Button>
+            </CardContent>
+          </div>
+         <BpModal
+         showBpModal={showBpModal}
+         setShowBpModal={setShowBpModal}>
+         </BpModal>
+        </Card>
+      }
+      {
+        <Card className="root">
+          <div className="details">
+            <CardContent className="content">
+            {errP && showErrMsg(errP)}
+        {successP && showSuccessMsg(successP)}
+              <Typography component="h5" variant="h5">
+                💓 Pulse Rate
+              </Typography>
+              <div className="margin">
+                <Grid container spacing={2} alignItems="flex-end">
+                  <Grid item>
+                    <FavoriteBorderIcon />
+                  </Grid>
+                  <Grid item>
+                    <TextField label="bpm" id="infoPulse" name="infoPulse" value={infoPulse} onChange={handleChangePulse} />
+                  </Grid>
+                  <IconButton aria-label="add" className="controls" onClick= {handleSubmitPulse}>
+                    <AddCircleOutlineRoundedIcon className="playIcon" />
+                  </IconButton>
+                 </Grid>
+              </div>
+              <Button className="summary_btn" onClick={openPulseModal}> Show History</Button>
+            </CardContent>
+          </div>
+          <PulseModal
+         showPulseModal={showPulseModal}
+         setShowPulseModal={setShowPulseModal}>
+         </PulseModal>
+        </Card>
+      }
+      {
+         <Card className="root">
+         <div className="details">
+           <CardContent className="content">
+           {errS && showErrMsg(errS)}
+        {successS && showSuccessMsg(successS)}
+             <Typography component="h5" variant="h5">
+               🎚 Sugar Level
+             </Typography>
+             <div className="margin">
+               <Grid container spacing={2} alignItems="flex-end">
+                 <Grid item>
+                   < HeightIcon />
+                 </Grid>
+                 <Grid item>
+                   <TextField label=" " id="infoSugar" name="infoSugar" value={infoSugar} onChange={handleChangeSugar} />
+                 </Grid>
+                 <IconButton aria-label="add" className="controls" onClick= {handleSubmitSugar} >
+                   <AddCircleOutlineRoundedIcon className="playIcon" />
+                 </IconButton>
+                </Grid>
+             </div>
+             <Button className="summary_btn" onClick={openSugarModal}> Show History</Button>
+           </CardContent>
+         </div>
+         <SugarModal
+         showSugarModal={showSugarModal}
+         setShowSugarModal={setShowSugarModal}>
+         </SugarModal>
+       </Card>
+      }
+    </div>
+  );
+}
+
+export default GeneralHealthInfo;
