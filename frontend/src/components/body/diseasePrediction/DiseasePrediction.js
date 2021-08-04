5345a62da5b7fbe19c79b2db899891dabea5ddc6
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-08-04 11:38:22 +0600

    user can see probable disease,percentage,suggested specialist

--- frontend/src/components/body/diseasePrediction/DiseasePrediction.js
+++ frontend/src/components/body/diseasePrediction/DiseasePrediction.js
@@ -1,43 +1,38 @@
 import React, { useState } from "react";
-import {
-  Grid,
-  Button,
-  Select,
-  MenuItem,
-  makeStyles,
- } from "@material-ui/core";
+import { Grid, Button, Select, MenuItem, makeStyles } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import "../../../static/Styling/diseasePrediction.css";
 import { getSymptomsList } from "./SymptomsList";
 import axios from "axios";
 import Prediction from "./Prediction";
 const initialState = {
-    s1: "",
-    s2: "",
-    s3: "",
-    s4: "",
-    s5: "",
-  };
-  const useStyles = makeStyles((theme) => ({
-   
-    selectEmpty: {
-      margin: theme.spacing(2),
-      minWidth: 320,
-      color: "#04272f",
-    },
-  }));
-  
+  s1: "",
+  s2: "",
+  s3: "",
+  s4: "",
+  s5: "",
+};
+const useStyles = makeStyles((theme) => ({
+  selectEmpty: {
+    margin: theme.spacing(2),
+    minWidth: 320,
+    color: "#04272f",
+  },
+}));
+
 export default function SymptomSelection() {
-    const token = useSelector((state) => state.token);
-    let arr = getSymptomsList();
+  const token = useSelector((state) => state.token);
+  let arr = getSymptomsList();
   const [symptom, setSymptom] = useState(initialState);
-  const [disease,setDisease]=useState('')
-  const { s1, s2, s3, s4, s5 } = symptom;
+  const [disease, setDisease] = useState([]);
+  const [percentage, setPercentage] = useState("");
+  const [doctor, setDoctor] = useState("");
+   const { s1, s2, s3, s4, s5 } = symptom;
   const handleChangeInput = (e) => {
     const { name, value } = e.target;
     setSymptom({ ...symptom, [name]: value });
   };
-  
+
   const symptomList = () => {
     let a = [];
     for (let i = 0; i < arr.length; i++) {
@@ -45,37 +40,39 @@ export default function SymptomSelection() {
     }
     return a;
   };
- 
-    const classes = useStyles();
+
+  const classes = useStyles();
   const handleSubmit = async (e) => {
     e.preventDefault();
-       await axios
+    await axios
       .get(
         "http://localhost:5000/disease-prediction",
-       
+
         {
-          headers: { Authorization: token ,s1: s1,
+          headers: {
+            Authorization: token,
+            s1: s1,
             s2: s2,
             s3: s3,
             s4: s4,
-            s5: s5,}
+            s5: s5,
+          },
         }
       )
       .then((res) => {
-         setDisease(res.data.a)
-        
-        console.log("disease", res.data.a);
+        setDisease(res.data.diseaseName);
+setDoctor(res.data.diseaseSpecialist)
+setPercentage(res.data.diseasePercenatge)
+        console.log("disease", res.data.diseaseSpecialist);
       });
   };
-  const showPrediction=async()=>{
-  return disease
-  }
-    return (
-        <div>
-             <h3>
+ 
+  return (
+    <div>
+      <div className='intro'>
         Identify possible conditions and treatments related to your symptoms{" "}
-      </h3>
-             <Grid className="root" container>
+      </div>
+      <Grid className="root" container>
         <Grid className="grid_symptoms" item xs={5}>
           Symptoms 1 :{" "}
           <Select
@@ -157,9 +154,7 @@ export default function SymptomSelection() {
           Predict
         </Button>
       </div>
-     <Prediction getPrediction={disease}/>
-    
-   </div>
-        
-    )
+      <Prediction getPrediction={disease} percent={percentage} specialist={doctor} />
+    </div>
+  );
 }
--- frontend/src/components/body/diseasePrediction/Prediction.js
+++ frontend/src/components/body/diseasePrediction/Prediction.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
-import {
-  InputLabel,
-} from "@material-ui/core";
+import { InputLabel } from "@material-ui/core";
 export default function Prediction(props) {
-const aa= props.getPrediction
+  const aa = props.getPrediction[0];
   return (
     <div>
       <InputLabel className="prediction_label">
@@ -12,30 +10,32 @@ const aa= props.getPrediction
           {" "}
           <Table hover size="md">
             <tr>
-              <th>#</th>
+              <th>#</th> &emsp;
               <th>Probable Disease</th>
-              &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
-              &emsp; &emsp; &emsp;
+              &emsp; &emsp; &emsp; &emsp; &emsp;
               <th>Suggested Specialist</th>
             </tr>
             <br />
             <br />
             <tr>
-              <td>1</td>
-               <td>{aa }</td>
-              {/*  <td>2 cups</td>
-                 */}
+              {" "}
+              <td>1</td> &emsp;
+              <td>
+                {aa} &emsp; &emsp; &emsp; {props.percent}{" "}
+              </td>
+              &emsp; &emsp; &emsp; &emsp; &emsp;
+              <td>&emsp;{props.specialist}</td>
             </tr>
             <br /> <br />{" "}
             <tr>
-              <td>2</td>
+              <td>2</td> &emsp;
               {/*   <td>Rice</td>
                 <td>2 cups</td>
                  */}
             </tr>
             <br /> <br />{" "}
             <tr>
-              <td>3</td>
+              <td>3</td> &emsp;
               {/*   <td>Rice</td>
                 <td>2 cups</td>
                  */}
