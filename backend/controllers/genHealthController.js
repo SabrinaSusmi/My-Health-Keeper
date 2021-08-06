b2d09e63083a57e19d9371224bfc3d41ef3fa1ac
Siana Rizwan <sianarizwan@gmail.com>
2021-08-07 02:04:51 +0600

    trying to fetch weight info

--- backend/controllers/genHealthController.js
+++ backend/controllers/genHealthController.js
@@ -15,4 +15,31 @@ const postHealthInfo = async (req,res) => {
 
 }
 
-module.exports = { postHealthInfo };
+const getWeightGraph = async (req,res)=>{
+    let user = req.user.id;
+    const weightdata = [];
+      await genHealthSchema.findOne({ user })
+        .then((response) => {
+         
+            const strDate = inputDate.getMonth();
+            const month = new Date().getMonth();
+  
+            if (strDate ==  month) {
+              weightdata.push(response.info);
+            } else {
+              console.log("no data found");
+            }
+          
+          if ( weightdata.length == 0) {
+            res.send();
+          } else {
+            res.send( weightdata);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.status(500).json({ msg: err.message });
+        });
+}
+
+module.exports = { postHealthInfo,getWeightGraph };
--- backend/routers/genHealth.route.js
+++ backend/routers/genHealth.route.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const auth=require("../middleware/auth");
 
-const {postHealthInfo} = require('../controllers/genHealthController');
+const {postHealthInfo,getWeightGraph} = require('../controllers/genHealthController');
 
 router.post('/addGenHealth',auth,postHealthInfo);
+router.get('/weightGraph',auth,getWeightGraph);
 
 module.exports=router;
