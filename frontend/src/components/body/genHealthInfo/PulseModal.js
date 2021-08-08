b295be7ca0e690af2041f235a046af41cb78ea7d
Siana Rizwan <sianarizwan@gmail.com>
2021-08-09 00:35:41 +0600

    Dummy graph added, data fetched from dummy api

--- frontend/src/components/body/genHealthInfo/PulseModal.js
+++ frontend/src/components/body/genHealthInfo/PulseModal.js
@@ -1,12 +1,52 @@
-import React, { useRef, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
 import "react-responsive-modal/styles.css";
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "@material-ui/core";
+import axios from "axios";
 
 
 const PulseModal = ({showPulseModal, setShowPulseModal}) => {
-    
+
+    const [chartData, setChartData] = useState({});
+  
+
+    const chart = () => {
+      let pulse_array = [];
+      let date_array = [];
+      axios
+        .get("http://dummy.restapiexample.com/api/v1/employees") //get info and input date from db
+        .then(res => {
+          console.log(res);
+          for (const dataObj of res.data.data) {
+            pulse_array.push(parseInt(dataObj.employee_salary)); //push data from db into the array
+            date_array.push(parseInt(dataObj.employee_age));
+          }
+          setChartData({
+            labels: date_array,
+            datasets: [
+              {
+                data:  pulse_array,
+                label: "Rainfall",
+                fill: false,
+                lineTension: 0.5,
+                backgroundColor: "rgba(75,192,192,1)",
+                borderColor: "rgba(0,0,0,1)",
+                borderWidth: 2,
+              }
+            ]
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      
+    };
+    useEffect(() => {
+      chart();
+    }, []);
+
     
     const closePulseModal = () => {
         setShowPulseModal(false);
@@ -27,6 +67,36 @@ const PulseModal = ({showPulseModal, setShowPulseModal}) => {
                     </Modal.Header>
                         <Modal.Body>
                         <h2>Graph</h2>
+
+                        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            title: { text: "Pulse Graph", display: true },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 10,
+                    beginAtZero: true
+                  },
+                  gridLines: {
+                    display: false
+                  }
+                }
+              ],
+              xAxes: [
+                {
+                  gridLines: {
+                    display: false
+                  }
+                }
+              ]
+            }
+          }}
+        />
+
                         </Modal.Body>
                         <Modal.Footer>
                             <Button variant="secondary"  onClick={() => {closePulseModal()}}>
