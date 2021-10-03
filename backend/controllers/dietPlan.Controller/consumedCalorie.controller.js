c66ad68b820303397299784f09ce98c8a8a999ac
SabrinaSusmi <sabrina.su512@gmail.com>
2021-10-03 15:39:30 +0600

    calorie saved successfully

--- backend/controllers/dietPlan.Controller/consumedCalorie.controller.js
+++ backend/controllers/dietPlan.Controller/consumedCalorie.controller.js
@@ -9,25 +9,43 @@ const postFood = async (req, res) => {
       food,
       quantity,
     } = req.body;
-    const calorie = calculateCalorie(food, quantity);
-    console.log("main"+calorie);
-  
-//     const foodItem = new consumedCalories({
-//       user,
-//       meal: meal,
-//       food,
-//       quantity: quantity,
-//       consumedCalories: calorie,
-//     });
-//     foodItem
-//       .save()
-//       .then((data) => {
-//         res.json(data);
-        
-//       })
-//       .catch((error) => {
-//         res.json(error);
-//       });
+    //const calorie = calculateCalorie(food, quantity);
+    //console.log("main"+calorie);
+    calorie_charts.find(
+      {
+        name: food,
+      }, (err, data) =>{
+          if(err) {
+              console.log(user);
+              console.log("Chart Data Food not found :" + err);
+          }
+          if(data) {
+              //console.log(data[0].calories);
+              foodCal = data[0].calories;
+              console.log(foodCal);
+              calorie = quantity*foodCal;
+              console.log(calorie);
+
+              const foodItem = new consumedCalories({
+                user,
+                meal: meal,
+                food,
+                quantity: quantity,
+                consumedCalories: calorie,
+              });
+              foodItem
+                .save()
+                .then((data) => {
+                  res.json(data);
+                  
+                })
+                .catch((error) => {
+                  res.json(error);
+                });
+              
+          }
+          
+      })
    };
 
   const calculateCalorie = (
