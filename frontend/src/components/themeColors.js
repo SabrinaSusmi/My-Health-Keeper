95d84e11665936457026d11f35cf48c085aed66e
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-10-06 20:44:55 +0600

    Create themeColors.js

---
+++ frontend/src/components/themeColors.js
@@ -0,0 +1,9 @@
+export const COLORS={
+ genHealthBackground:'#c1eafa'   ,
+ dietBackground:'#c1eafa',
+reportsBackground:"#c1eafa",
+spHealthBackground:'#c1eafa',
+medicineBackground:'#c1eafa',
+diseasePrediction:'#c1eafa',
+menstrualBackground:'#c1eafa',
+}
