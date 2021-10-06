5dc541435d21697b0fc853ff7a4efe746ca126dc
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-10-07 03:04:47 +0600

    genhealth BP backend updated

--- backend/controllers/genHealthController.js
+++ backend/controllers/genHealthController.js
@@ -1,169 +1,296 @@
-const genHealthSchema = require('../models/genHealth.model');
+const genHealthSchema = require("../models/genHealth.model");
 
-const postHealthInfo = async (req,res) => {
-    let user = req.user.id;
+const postHealthInfo = async (req, res) => {
+  let user = req.user.id;
 
-    const { infoTitle, info } = req.body;
-    //console.log(new Date().toISOString().slice(0, 10))
+  const { infoTitle, info } = req.body;
+  //console.log(new Date().toISOString().slice(0, 10))
 
-    genHealthSchema.findOne({ infoTitle : infoTitle, inputDate : new Date().toISOString().slice(0, 10) }).then((health) => {
-        if(health) {
-            genHealthSchema.findOneAndUpdate({infoTitle : infoTitle, inputDate : new Date().toISOString().slice(0, 10) }, {info})
-            .then(data => res.json(data)).catch(err => res.json(err))
+  if (infoTitle === "Bp") {
+    let bp_split = info.split("/");
+    genHealthSchema
+      .findOne({
+        infoTitle: infoTitle,
+        inputDate: new Date().toISOString().slice(0, 10),
+      })
+      .then((health) => {
+        if (health) {
+          genHealthSchema
+            .findOneAndUpdate(
+              {
+                infoTitle: infoTitle,
+                inputDate: new Date().toISOString().slice(0, 10),
+                bpType: "systolic",
+              },
+              { info: bp_split[0] }
+            )
+            .then((data) => res.json(data))
+            .catch((err) => res.json(err));
+          genHealthSchema
+            .findOneAndUpdate(
+              {
+                infoTitle: infoTitle,
+                inputDate: new Date().toISOString().slice(0, 10),
+                bpType: "diastolic",
+              },
+              { info: bp_split[1] }
+            )
+            .then((data) => res.json(data))
+            .catch((err) => res.json(err));
         } else {
-            const healthInfo = new genHealthSchema ({
-                user,
-                infoTitle : infoTitle,
-                info : info,
-                inputDate : new Date().toISOString().slice(0, 10)
-            });
-            healthInfo.save().then((data)=> res.json(data) ).catch((err)=> res.json(err));
+          const sysInfo = new genHealthSchema({
+            user,
+            infoTitle: infoTitle,
+            bpType: "systolic",
+            info: bp_split[0],
+            inputDate: new Date().toISOString().slice(0, 10),
+          });
+          sysInfo
+            .save()
+            .then((data) => res.json(data))
+            .catch((err) => res.json(err));
+
+          const diasInfo = new genHealthSchema({
+            user,
+            infoTitle: infoTitle,
+            bpType: "diastolic",
+            info: bp_split[1],
+            inputDate: new Date().toISOString().slice(0, 10),
+          });
+          diasInfo
+            .save()
+            .then((data) => {
+              console.log("ddqdwssdx");
+              res.json(data);
+            })
+            .catch((err) => res.json(err));
         }
-    }).catch(err => res.json(err))
+      })
+      .catch((err) => res.json(err));
+  } else {
+    genHealthSchema
+      .findOne({
+        infoTitle: infoTitle,
+        inputDate: new Date().toISOString().slice(0, 10),
+      })
+      .then((health) => {
+        if (health) {
+          genHealthSchema
+            .findOneAndUpdate(
+              {
+                infoTitle: infoTitle,
+                inputDate: new Date().toISOString().slice(0, 10),
+              },
+              { info }
+            )
+            .then((data) => res.json(data))
+            .catch((err) => res.json(err));
+        } else {
+          const healthInfo = new genHealthSchema({
+            user,
+            infoTitle: infoTitle,
+            info: info,
+            inputDate: new Date().toISOString().slice(0, 10),
+          });
+          healthInfo
+            .save()
+            .then((data) => {
+              console.log("asedqkwisjiw");
+              res.json(data);
+            })
+            .catch((err) => res.json(err));
+        }
+      })
+      .catch((err) => res.json(err));
+  }
+};
 
+const getSugarData = async (req, res) => {
+  let user = req.user.id;
+  let health_data = [];
+  let chart_data = [];
+  let chart_date = [];
+  const fromDate = new Date();
+  fromDate.setDate(fromDate.getDate() - 7);
 
+  genHealthSchema.find(
+    {
+      user: user,
+      infoTitle: "Sugar",
+      inputDate: { $gte: fromDate },
+    },
+    (err, data) => {
+      if (err) {
+        console.log(user);
+        console.log("Chart Data :" + err);
+      }
+      if (data) {
+        health_data = data;
+        //  console.log(health_data);
+        health_data.forEach((cdata) => {
+          chart_data.push(parseInt(cdata.info));
+          chart_date.push(cdata.inputDate.toISOString().slice(0, 10));
+        });
 
-}
+        // console.log(chart_date)
+        // console.log(chart_data)
 
-const getSugarData = async (req,res)=>{
-    let user = req.user.id;
-    let health_data = [];
-    let chart_data = [];
-    let chart_date = [];
-    const fromDate=new Date()
-    fromDate.setDate(fromDate.getDate()-7)
+        res.send({ infoData: chart_data, dates: chart_date });
+      }
+    }
+  );
+};
 
-    genHealthSchema.find(
-        {
-          user: user,
-          infoTitle: "Sugar",
-          inputDate: {$gte:fromDate},
-        }, (err, data) =>{
-            if(err) {
-                console.log(user);
-                console.log("Chart Data :" + err);
-            }
-            if(data) {
-                health_data = data;
-              //  console.log(health_data);
-                health_data.forEach(cdata=>{
-                    chart_data.push(cdata.info)
-                    chart_date.push(cdata.inputDate.toISOString().slice(0, 10))
-                });
+const getWeightData = async (req, res) => {
+  let user = req.user.id;
+  let health_data = [];
+  let chart_data = [];
+  let chart_date = [];
+  const fromDate = new Date();
+  fromDate.setDate(fromDate.getDate() - 7);
 
-                // console.log(chart_date)
-                // console.log(chart_data)
+  genHealthSchema.find(
+    {
+      user: user,
+      infoTitle: "Weight",
+      inputDate: { $gte: fromDate },
+    },
+    (err, data) => {
+      if (err) {
+        console.log(user);
+        console.log("Chart Data :" + err);
+      }
+      if (data) {
+        health_data = data;
+        //  console.log(health_data);
+        health_data.forEach((cdata) => {
+          chart_data.push(parseInt(cdata.info));
+          chart_date.push(cdata.inputDate.toISOString().slice(0, 10));
+        });
 
-                res.send({infoData : chart_data, dates : chart_date});
-            }
-            
-        })
-}
+        // console.log(chart_date)
+        // console.log(chart_data)
 
-const getWeightData = async (req,res)=>{
-    let user = req.user.id;
-    let health_data = [];
-    let chart_data = [];
-    let chart_date = [];
-    const fromDate=new Date()
-    fromDate.setDate(fromDate.getDate()-7)
+        res.send({ infoData: chart_data, dates: chart_date });
+      }
+    }
+  );
+};
 
-    genHealthSchema.find(
-        {
-          user: user,
-          infoTitle: "Weight",
-          inputDate: {$gte:fromDate},
-        }, (err, data) =>{
-            if(err) {
-                console.log(user);
-                console.log("Chart Data :" + err);
-            }
-            if(data) {
-                health_data = data;
-              //  console.log(health_data);
-                health_data.forEach(cdata=>{
-                    chart_data.push(cdata.info)
-                    chart_date.push(cdata.inputDate.toISOString().slice(0, 10))
-                });
+const getBpSysData = async (req, res) => {
+  let user = req.user.id;
+  let health_data = [];
+  let chart_data = [];
+  let chart_date = [];
+  const fromDate = new Date();
+  fromDate.setDate(fromDate.getDate() - 7);
 
-                // console.log(chart_date)
-                // console.log(chart_data)
+  genHealthSchema.find(
+    {
+      user: user,
+      infoTitle: "Bp",
+      inputDate: { $gte: fromDate },
+      bpType: "systolic",
+    },
+    (err, data) => {
+      if (err) {
+        console.log(user);
+        console.log("Chart Data :" + err);
+      }
+      if (data) {
+        health_data = data;
 
-                res.send({infoData : chart_data, dates : chart_date});
-            }
-            
-        })
-}
+        health_data.forEach((cdata) => {
+          chart_data.push(cdata.info);
+          chart_date.push(cdata.inputDate.toISOString().slice(0, 10));
+        });
 
-const getBpData = async (req,res)=>{
-    let user = req.user.id;
-    let health_data = [];
-    let chart_data = [];
-    let chart_date = [];
-    const fromDate=new Date()
-    fromDate.setDate(fromDate.getDate()-7)
+        // console.log(chart_date)
+        // console.log(chart_data)
 
-    genHealthSchema.find(
-        {
-          user: user,
-          infoTitle: "Bp",
-          inputDate: {$gte:fromDate},
-        }, (err, data) =>{
-            if(err) {
-                console.log(user);
-                console.log("Chart Data :" + err);
-            }
-            if(data) {
-                health_data = data;
-               // console.log(health_data);
-                health_data.forEach(cdata=>{
-                    chart_data.push(cdata.info)
-                    chart_date.push(cdata.inputDate.toISOString().slice(0, 10))
-                });
+        res.send({ infoData: chart_data, dates: chart_date });
+      }
+    }
+  );
+};
+const getBpDiasData = async (req, res) => {
+  let user = req.user.id;
+  let health_data = [];
+  let chart_data = [];
+  let chart_date = [];
+  const fromDate = new Date();
+  fromDate.setDate(fromDate.getDate() - 7);
 
-                // console.log(chart_date)
-                // console.log(chart_data)
+  genHealthSchema.find(
+    {
+      user: user,
+      infoTitle: "Bp",
+      inputDate: { $gte: fromDate },
+      bpType: "diastolic",
+    },
+    (err, data) => {
+      if (err) {
+        console.log(user);
+        console.log("Chart Data :" + err);
+      }
+      if (data) {
+        health_data = data;
 
-                res.send({infoData : chart_data, dates : chart_date});
-            }
-            
-        })
-}
+        health_data.forEach((cdata) => {
+          chart_data.push(cdata.info);
+          chart_date.push(cdata.inputDate.toISOString().slice(0, 10));
+        });
 
-const getPulseData = async (req,res)=>{
-    let user = req.user.id;
-    let health_data = [];
-    let chart_data = [];
-    let chart_date = [];
-    const fromDate=new Date()
-    fromDate.setDate(fromDate.getDate()-7)
+        // console.log(chart_date)
+        // console.log(chart_data)
 
-    genHealthSchema.find(
-        {
-          user: user,
-          infoTitle: "Pulse",
-          inputDate: {$gte:fromDate},
-        }, (err, data) =>{
-            if(err) {
-                console.log(user);
-                console.log("Chart Data :" + err);
-            }
-            if(data) {
-                health_data = data;
-               // console.log(health_data);
-                health_data.forEach(cdata=>{
-                    chart_data.push(cdata.info)
-                    chart_date.push(cdata.inputDate.toISOString().slice(0, 10))
-                });
+        res.send({ infoData: chart_data, dates: chart_date });
+      }
+    }
+  );
+};
 
-                // console.log(chart_date)
-                // console.log(chart_data)
+const getPulseData = async (req, res) => {
+  let user = req.user.id;
+  let health_data = [];
+  let chart_data = [];
+  let chart_date = [];
+  const fromDate = new Date();
+  fromDate.setDate(fromDate.getDate() - 7);
 
-                res.send({infoData : chart_data, dates : chart_date});
-            }
-            
-        })
-}
+  genHealthSchema.find(
+    {
+      user: user,
+      infoTitle: "Pulse",
+      inputDate: { $gte: fromDate },
+    },
+    (err, data) => {
+      if (err) {
+        console.log(user);
+        console.log("Chart Data :" + err);
+      }
+      if (data) {
+        health_data = data;
+        // console.log(health_data);
+        health_data.forEach((cdata) => {
+          chart_data.push(parseInt(cdata.info));
+          chart_date.push(cdata.inputDate.toISOString().slice(0, 10));
+        });
 
+        // console.log(chart_date)
+        // console.log(chart_data)
 
-module.exports = { postHealthInfo, getSugarData, getWeightData, getBpData, getPulseData };
+        res.send({ infoData: chart_data, dates: chart_date });
+      }
+    }
+  );
+};
+
+module.exports = {
+  postHealthInfo,
+  getSugarData,
+  getWeightData,
+  getBpSysData,
+  getBpDiasData,
+  getPulseData,
+};
--- backend/routers/genHealth.route.js
+++ backend/routers/genHealth.route.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const auth=require("../middleware/auth");
+const auth = require("../middleware/auth");
 
-const {postHealthInfo,getSugarData, getWeightData, getBpData, getPulseData} = require('../controllers/genHealthController');
+const {
+  postHealthInfo,
+  getSugarData,
+  getWeightData,
+  getBpSysData,
+  getBpDiasData,
+  getPulseData,
+} = require("../controllers/genHealthController");
 
-router.post('/addGenHealth',auth,postHealthInfo);
+router.post("/addGenHealth", auth, postHealthInfo);
 // router.get('/getChart/:title',auth,getChartData);
-router.get('/getChart/Sugar',auth,getSugarData);
-router.get('/getChart/Weight',auth,getWeightData);
-router.get('/getChart/Bp',auth,getBpData);
-router.get('/getChart/Pulse',auth,getPulseData);
+router.get("/getChart/Sugar", auth, getSugarData);
+router.get("/getChart/Weight", auth, getWeightData);
+router.get("/getChart/Bp_sys", auth, getBpSysData);
+router.get("/getChart/Bp_dias", auth, getBpDiasData);
+router.get("/getChart/Pulse", auth, getPulseData);
 
-module.exports=router;
+module.exports = router;
