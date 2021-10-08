053f1afcf4943d286f70040578ace09cbd5aac18
SabrinaSusmi <sabrina.su512@gmail.com>
2021-10-08 12:22:19 +0600

    Add and update daily calorie completed

--- backend/controllers/dietPlan.Controller/consumedCalorie.controller.js
+++ backend/controllers/dietPlan.Controller/consumedCalorie.controller.js
@@ -1,6 +1,8 @@
 const consumedCalories = require("../../models/consumedCalories.model");
 const calorie_charts = require("../../models/caloriesChart.models");
 const DietTargetModel = require("../../models/diet.targetModel");
+const dailyCalorie = require("../../models/dailyCalorie");
+const { findOne } = require("../../models/consumedCalories.model");
 
 const postFood = async (req, res) => {
     let user = req.user.id;
@@ -12,6 +14,7 @@ const postFood = async (req, res) => {
     } = req.body;
     //const calorie = calculateCalorie(food, quantity);
     //console.log("main"+calorie);
+    
     calorie_charts.find(
       {
         name: food,
@@ -23,7 +26,7 @@ const postFood = async (req, res) => {
           if(data) {
               //console.log(data[0].calories);
               foodCal = data[0].calories;
-              console.log(foodCal);
+              //console.log(foodCal);
               calorie = quantity*foodCal;
               console.log(calorie);
 
@@ -42,6 +45,33 @@ const postFood = async (req, res) => {
                 foodItem
                   .save()
                   .then((data) => {
+                    dailyCalorie.findOne({
+                      user : user,
+                      date: new Date().toISOString().slice(0, 10),
+                    })
+                    .then((dailyCal) => {
+                      if (dailyCal) {
+                        cal = dailyCal.consumedCalories;
+                        newCal = cal+calorie;
+                        dailyCalorie.findOneAndUpdate({
+                          user : user,
+                          date: new Date().toISOString().slice(0, 10),
+                        },
+                          { consumedCalories: newCal })
+                        .then((data) => console.log("Daily calorie updated"))
+                        .catch((err) => console.log("Daily calorie update error : "+err));
+                      }else {
+                        const dailyConsumed = new dailyCalorie({
+                          user,
+                          consumedCalories: calorie,
+                          requiredCalories:reqCal,
+                          date : new Date().toISOString().slice(0, 10),
+                        })
+                        dailyConsumed.save().then(() => {
+                          console.log("Data saved in daily calorie table")
+                        }).catch((err) => console.log("Daily calorie entry error : "+err));
+                      }
+                    })
                     console.log("wxdwd ", reqcal)
                     res.json(data);
                     
