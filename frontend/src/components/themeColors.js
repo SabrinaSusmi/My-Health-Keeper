b572a8832b9f87d785c1e55e52ca42b81ea829c5
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-10-09 17:44:12 +0600

    Update themeColors.js

--- frontend/src/components/themeColors.js
+++ frontend/src/components/themeColors.js
@@ -2,7 +2,7 @@ export const COLORS={
  genHealthBackground:'#c1eafa'   ,
  dietBackground:'#c1eafa',
 reportsBackground:"#c1eafa",
-spHealthBackground:'#c1eafa',
+spHealthBackground:'#b3d3a6',
 medicineBackground:'#c1eafa',
 diseasePrediction:'#c1eafa',
 menstrualBackground:'#c1eafa',
