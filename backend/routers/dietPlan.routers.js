d745a0d41e2a8a2b475c5a9dcc0adcb9741acf65
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-10-11 21:15:17 +0600

    diet food table ui modified, consumed calorie bug

--- backend/routers/dietPlan.routers.js
+++ backend/routers/dietPlan.routers.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth=require("../middleware/auth");
 
 const saveTargetInfo=require('../controllers/dietPlan.Controller/dietPlan.controllers')
-const { postFood, getFood, deleteFood, getFoodMenu } = require('../controllers/dietPlan.Controller/consumedCalorie.controller')
+const { postFood, getFood, deleteFood, getFoodMenu ,getDietSummaryOfTheDay} = require('../controllers/dietPlan.Controller/consumedCalorie.controller')
 const getMonthlyDietData =require('../controllers/dietPlan.Controller/dietProgress.controller')
 
 router.post("/setup-target_info",saveTargetInfo);
@@ -14,4 +14,5 @@ router.delete('/foodList/delete/:id',auth, deleteFood);
 router.get("/getFoodMenu",auth, getFoodMenu);
 
 router.get('/get_monthly_diet_data',auth,getMonthlyDietData)
+router.get('/getDietSummaryOfTheDay',auth,getDietSummaryOfTheDay)
 module.exports = router;
--- frontend/src/components/body/dietPlan/FoodItemTable.js
+++ frontend/src/components/body/dietPlan/FoodItemTable.js
@@ -1,32 +1,48 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import Table from "react-bootstrap/Table";
-import {IconButton,Button} from "@material-ui/core";
+import { IconButton, Button } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
-import EditIcon from '@material-ui/icons/Edit';
-import AddCircleOutlineRoundedIcon from "@material-ui/icons/AddCircleOutlineRounded";
-import AddFoodModal from "./addFoodModal";
-import '../../../static/Styling/diet.foodItemTable.css'
+import "../../../static/Styling/dietFoodTable.css";
+import "../../../static/Styling/diet.foodItemTable.css";
 // import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 import { useSelector } from "react-redux";
 import axios from "axios";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
 
 export default function FoodItemTable() {
   const token = useSelector((state) => state.token);
-    
-  const [showFoodModal, setShowFoodModal] = useState(false);
-  const openFoodModal = () => setShowFoodModal(true);
-
+  const [multipleProgress, setMultipleProgress] = useState(0.0);
   const [foodList, setFoodList] = useState([]);
-
+  const [consumed, setConsumed] = useState("");
+  const [required, setRequired] = useState("");
+  const [remaining, setRemaining] = useState("");
   const getFoodConsumed = async () => {
     await axios
       .get("http://localhost:5000/diet-plan/getFoodList", {
         headers: { Authorization: token },
       })
-      .then((res) => setFoodList(res.data));
+      .then((res) => {setFoodList(res.data)});
+  };
+  const getDietSummaryOfTheDay = async () => {
+    await axios
+      .get("http://localhost:5000/diet-plan/getDietSummaryOfTheDay", {
+        headers: { Authorization: token },
+      })
+      .then((res) => {
+
+        setConsumed(res.data.consumedCalories);
+        setRequired(res.data.requiredCalories);
+        setRemaining(res.data.requiredCalories - res.data.consumedCalories);
+        const percentage = (parseFloat(res.data.consumedCalories / res.data.requiredCalories).toFixed(4))*100;
+setMultipleProgress(percentage)
+        console.log(res.data);
+      });
   };
   useEffect(async () => {
-    getFoodConsumed(); 
+    getFoodConsumed();
+    // getDietSummaryOfTheDay();
   }, []);
 
   const deleteFood = async (id) => {
@@ -38,63 +54,95 @@ export default function FoodItemTable() {
         console.log(response.data);
       });
 
-    const removedFood = [...foodList].filter(
-      (el) => el._id !== id
-    );
+    const removedFood = [...foodList].filter((el) => el._id !== id);
     setFoodList(removedFood);
   };
 
   return (
-      <div>
-   
+    <div>
       <div className="food_table">
-
-        <div>
-       
-         
-
+        <div className="diet_info_section">
+          <div className="diet_info_item">
+            Required
+            <br></br>
+            {required} kcal
+          </div>
+          <div className="diet_info_item">
+            Consumed
+            <br></br>
+            {consumed} kcal
+          </div>
+          <div className="diet_info_item">
+            
+             <br></br>
+            <p style={remaining < 0 ? { color: "red" } : { color: "#373950" }}>
+            Remaining  {remaining} kcal
+            </p>
+          </div>
+          <div style={{backgroundColor:'white', width: 100, height: 100, borderRadius:'50%',boxShadow: '2px 2px 2px 2px #373950' }}>
+              <CircularProgressbar
+              
+                value={multipleProgress}
+                text={`${multipleProgress}%`}
+                styles={buildStyles({
+                  rotation: 0.25,
+                  strokeLinecap: "butt",
+                  textSize: "16px",
+                  pathTransitionDuration: 0.5,
+                  pathColor: `rgba(55, 57, 80, ${multipleProgress / 100})`,
+                  textColor: "#373950",
+                  textSize:25,
+                  trailColor: "white",
+                  backgroundColor: "#8fce00",
+                  
+                })}
+              />
+            </div>
         </div>
-      
+        <div className="diet_info_item_progress"></div>
         <Table hover size="md">
           <thead>
-            <tr style={{background:'#eae6e6da'}}>
-            <th>Meal Description</th>
-             <th>Item Name</th>
+            <tr style={{ background: "#eae6e6da" }}>
+              <th>Meal Description</th>
+              <th>Item Name</th>
               <th>Quantity (servings)</th>
-             
+
               <th>Calories Consumed (kcal)</th>
-             
+
               <th></th>
             </tr>
           </thead>
-          
+
           <tbody>
-          {foodList.map((food,index) => (
-                    <tr style={index % 2? { background : "#f8db57",   }:{ background : "#f4c805" }} >
-                      <td>{food.meal}</td>
-                      <td>{food.food}</td>
-                      <td>{food.quantity}</td>
-                      
-                      <td>{food.consumedCalories}</td>
-                      
-                      <td>
-                      <IconButton
-                        className="btn"
-                        data-toggle="tooltip"
-                        title="Delete"
-                        onClick={() => deleteFood(food._id)}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                      </td>
-                      
-                    </tr>
-                    
-                  ))}
+            {foodList.map((food, index) => (
+              <tr
+                style={
+                  index % 2
+                    ? { background: "#f8db57" }
+                    : { background: "#f4c805" }
+                }
+              >
+                <td>{food.meal}</td>
+                <td>{food.food}</td>
+                <td>{food.quantity}</td>
+
+                <td>{food.consumedCalories}</td>
+
+                <td>
+                  <IconButton
+                    className="btn"
+                    data-toggle="tooltip"
+                    title="Delete"
+                    onClick={() => deleteFood(food._id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </div>
-        
-      </div>
-  )
+    </div>
+  );
 }
