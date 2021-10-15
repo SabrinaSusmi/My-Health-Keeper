77a43430dd8ddb20e9d329758ab8729096ca5c1d
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-10-15 21:26:25 +0600

    genHealth frontend added

---
+++ frontend/src/components/body/genHealthInfo/GenHealthHistory.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import "../../../static/Styling/dietPlan.css";
+import "../../../static/Styling/healthInfo.css";
+import "react-responsive-modal/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Table from "react-bootstrap/Table";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { TextField } from "@material-ui/core";
+
+function GenHealthHistoryTable() {
+    
+  const token = useSelector((state) => state.token);
+  const [healthInfo, setHealthInfo] = useState([]);
+
+  const getHealthDetailsTable = async (e) => {
+    e.preventDefault();
+    const historyDate = e.target.value;
+   
+
+    await axios
+      .get("http://localhost:5000/genHealthHistory", {
+        headers: { Authorization: token, dates: historyDate },
+      })
+      .then((res) => {
+        setHealthInfo(res.data);
+      });
+  };
+  
+  return (
+    <div>
+    <pre></pre>
+      <pre></pre>
+      <div style={{color:'#155844',marginLeft:'30%',  marginTop:'3%',fontSize:20,fontWeight:'bold'}}> 📅 Select Date for Viewing Your General Health Information Details </div>
+      <TextField
+        style={{color:'#155844',marginLeft:'30%',  marginTop:'3%'}}
+        variant="outlined"
+        required
+        id="startdate"
+        name="startdate"
+        onChange={getHealthDetailsTable}
+        type="date"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <pre></pre>
+      <pre></pre>
+      <pre></pre>
+      <hr></hr>
+      <div className="food_table" style={{marginLeft:"10%",marginRight:"10%"}}>
+        <div className="diet_info_item_progress"></div>
+        <Table hover size="sm">
+          <thead>
+            <tr style={{ background: "transparent" }}>
+              <th>Title</th>
+              <th>Information Type </th>
+              <th>Information / Data</th>
+              <th></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {healthInfo.map((data, index) => (
+              <tr
+                style={index % 2 ? { color: "#0777c2" } : { color: "#f7900a" }}
+              >
+                <td>{data.infoTitle}</td>
+                <td>{data.bpType}</td>
+                <td>{data.info}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+export default GenHealthHistoryTable;
