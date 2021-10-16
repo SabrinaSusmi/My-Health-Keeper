960d07a5d2b8287142da2d3f19b66236e6eae8bf
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-10-16 15:10:09 +0600

    Update BurnedCalorie.js

--- frontend/src/components/body/dietPlan/BurnedCalorie.js
+++ frontend/src/components/body/dietPlan/BurnedCalorie.js
@@ -3,19 +3,9 @@ import { Button, Link, TextField, Select, IconButton } from "@material-ui/core";
 import "../../../static/Styling/dietProgress.css";
 import { useSelector } from "react-redux";
 import axios from "axios";
-import { Line } from "react-chartjs-2";
-import VisibilityIcon from "@material-ui/icons/Visibility";
-
-import { ShowHeader } from "../../header/Header";
-import { ShowFeatureButtons } from "../../header/featureButton";
-
-import { Container, Row, Col } from "react-grid-system";
 
 import { COLORS } from "../../themeColors";
 
-const initialState = {
-  selectedMonth: "",
-};
 
 export default function BurnedCalories() {
     const [burnCal, setBurnCal] = useState("");
@@ -24,7 +14,9 @@ export default function BurnedCalories() {
   const token = useSelector((state) => state.token);
   const saveBurnCal = async (e) => {
     e.preventDefault();
-    
+    await axios.post('http://localhost:5000/diet-plan/burn_calorie',{burnedCalories: burnCal, }, {
+        headers: { Authorization: token  }
+      }).then((res)=>{console.log(res.data)}).catch((err)=>{console.log(err)})
   };
   
 
@@ -38,8 +30,8 @@ Track the amount of calories you've burn everyday
 <br></br>
               <TextField
                 type="text"
-                id="calorieBurn"
-                name="calorieBurn"
+                id="burnedCalories"
+                name="burnedCalories"
                 label="Kcal"
                 value={burnCal}
                 onChange={(e) => {
