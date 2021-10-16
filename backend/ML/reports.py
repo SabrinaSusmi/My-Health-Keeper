c5af2952c74d0c18df39d3c2b24ac9b9b743a31c
SabrinaSusmi <sabrina.su512@gmail.com>
2021-10-17 02:11:49 +0600

    reports file bug solved

--- backend/ML/reports.py
+++ backend/ML/reports.py
@@ -9,12 +9,12 @@ from keras.models import load_model
 
 
 # load model
-model = load_model('./xray_model.h5')
+model = load_model('../backend/ML/xray_model.h5')
 # summarize model.
-model.summary()
+print(model.summary())
 
-load_image = cv2.imread('./backend/ML/person1_virus_7.jpeg')
+load_image = cv2.imread('../backend/ML/person1_virus_7.jpeg')
 img_new = cv2.resize(load_image, (180,180))
 new = img_new.reshape(1,img_new.shape[0], img_new.shape[1],3)
-result = new_model.predict(new, steps=10)
+result = model.predict(new, steps=10)
 print(result[0][0])
