c51d787977c116f6fad8a7d7a68004c55fd242ca
Siana Rizwan <sianarizwan@gmail.com>
2021-10-16 18:42:05 +0600

    Calorie Burned added

--- backend/routers/dietPlan.routers.js
+++ backend/routers/dietPlan.routers.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth=require("../middleware/auth");
 
 const saveTargetInfo=require('../controllers/dietPlan.Controller/dietPlan.controllers')
-const { postFood, getFood, deleteFood, getFoodMenu ,getDietSummaryOfTheDay,getFoodHistory} = require('../controllers/dietPlan.Controller/consumedCalorie.controller')
+const { postFood, getFood, deleteFood, getFoodMenu ,getDietSummaryOfTheDay,getFoodHistory, burnCalorie} = require('../controllers/dietPlan.Controller/consumedCalorie.controller')
 const getMonthlyDietData =require('../controllers/dietPlan.Controller/dietProgress.controller')
 
 router.post("/setup-target_info",saveTargetInfo);
@@ -12,7 +12,9 @@ router.get("/getFoodList",auth,getFood);
 router.delete('/foodList/delete/:id',auth, deleteFood);
 
 router.get("/getFoodMenu",auth, getFoodMenu);
+
 router.get("/getFoodHistory",auth,getFoodHistory)
 router.get('/get_monthly_diet_data',auth,getMonthlyDietData)
 router.get('/getDietSummaryOfTheDay',auth,getDietSummaryOfTheDay)
+router.post('/burn_calorie',auth,burnCalorie)
 module.exports = router;
