e6b89997a85a0031a9944fc5ab5b544ad7d13f15
SabrinaSusmi <sabrina.su512@gmail.com>
2021-10-17 13:13:43 +0600

    genhealth controller updated

--- backend/controllers/GenHealthController/genHealthController.js
+++ backend/controllers/GenHealthController/genHealthController.js
@@ -1,114 +1,6 @@
-const genHealthSchema = require("../models/genHealth.model");
+const genHealthSchema = require("../../models/genHealth.model");
 
-const postHealthInfo = async (req, res) => {
-  let user = req.user.id;
 
-  const { infoTitle, info } = req.body;
-  //console.log(new Date().toISOString().slice(0, 10))
-
-  if (infoTitle === "Bp") {
-    let bp_split = info.split("/");
-    genHealthSchema
-      .findOne({
-        user:user,
-        infoTitle: infoTitle,
-        inputDate: new Date().toISOString().slice(0, 10),
-      })
-      .then((health) => {
-        if (health) {
-          genHealthSchema
-            .findOneAndUpdate(
-              {
-                user:user,
-                infoTitle: infoTitle,
-                inputDate: new Date().toISOString().slice(0, 10),
-                bpType: "systolic",
-              },
-              { info: bp_split[0] }
-            )
-            .then((data) => res.json(data))
-            .catch((err) => res.json(err));
-          genHealthSchema
-            .findOneAndUpdate(
-              {
-                user:user,
-                infoTitle: infoTitle,
-                inputDate: new Date().toISOString().slice(0, 10),
-                bpType: "diastolic",
-              },
-              { info: bp_split[1] }
-            )
-            .then((data) => res.json(data))
-            .catch((err) => res.json(err));
-        } else {
-          const sysInfo = new genHealthSchema({
-            user,
-            infoTitle: infoTitle,
-            bpType: "systolic",
-            info: bp_split[0],
-            inputDate: new Date().toISOString().slice(0, 10),
-          });
-          sysInfo
-            .save()
-            .then((data) => res.json(data))
-            .catch((err) => res.json(err));
-
-          const diasInfo = new genHealthSchema({
-            user,
-            infoTitle: infoTitle,
-            bpType: "diastolic",
-            info: bp_split[1],
-            inputDate: new Date().toISOString().slice(0, 10),
-          });
-          diasInfo
-            .save()
-            .then((data) => {
-              console.log("ddqdwssdx");
-              res.json(data);
-            })
-            .catch((err) => res.json(err));
-        }
-      })
-      .catch((err) => res.json(err));
-  } else {
-    genHealthSchema
-      .findOne({
-        user:user,
-        infoTitle: infoTitle,
-        inputDate: new Date().toISOString().slice(0, 10),
-      })
-      .then((health) => {
-        if (health) {
-          genHealthSchema
-            .findOneAndUpdate(
-              {
-                user:user,
-                infoTitle: infoTitle,
-                inputDate: new Date().toISOString().slice(0, 10),
-              },
-              { info }
-            )
-            .then((data) => res.json(data))
-            .catch((err) => res.json(err));
-        } else {
-          const healthInfo = new genHealthSchema({
-            user,
-            infoTitle: infoTitle,
-            info: info,
-            inputDate: new Date().toISOString().slice(0, 10),
-          });
-          healthInfo
-            .save()
-            .then((data) => {
-              console.log("asedqkwisjiw");
-              res.json(data);
-            })
-            .catch((err) => res.json(err));
-        }
-      })
-      .catch((err) => res.json(err));
-  }
-};
 
 const getSugarData = async (req, res) => {
   let user = req.user.id;
@@ -304,20 +196,21 @@ const getHistory = async (req, res) => {
         if (dates == dbDate) {
           infolist.push(data);
         }
-        
       });
       console.log(infolist[0]);
       res.send(infolist);
     }
   });
-}
+};
+
 
 module.exports = {
-  postHealthInfo,
+  
   getSugarData,
   getWeightData,
   getBpSysData,
   getBpDiasData,
   getPulseData,
   getHistory,
+ 
 };
