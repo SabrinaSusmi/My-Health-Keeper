4c370af21852a93bc4048624eb444d1c7ad7ae72
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-10-17 21:15:53 +0600

    bug solved

--- frontend/src/components/body/dietPlan/DietOverview.js
+++ frontend/src/components/body/dietPlan/DietOverview.js
@@ -27,7 +27,7 @@ export default function DietOverview() {
         headers: { Authorization: token },
       })
       .then((res) => {
-        setConsumed(res.data.consumedCalories);
+        setConsumed(res.data.consumedCalories-res.data.burnedCalories);
         setRequired(res.data.requiredCalories);
         setRemaining(res.data.requiredCalories - res.data.consumedCalories);
         const percentage =
