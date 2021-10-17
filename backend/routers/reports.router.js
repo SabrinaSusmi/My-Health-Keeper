d5d8dd33bf6fb021391cbfd8d4407ec0baacbf26
Siana Rizwan <sianarizwan@gmail.com>
2021-10-17 09:07:00 +0600

    Create reports.router.js

---
+++ backend/routers/reports.router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { reports } = require("../controllers/reports.controller");
+const { upload } = require("../utilities/filehelper");
+
+router.get('/reports',upload.single("file"), reports)
+
+module.exports = router;
