0492ebbece3b7fa6e5602fbc401da6aa9fa0685d
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-10-17 13:32:52 +0600

    reports backend file problem

--- backend/routers/reports.router.js
+++ backend/routers/reports.router.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { reports } = require("../controllers/reports.controller");
 const { upload } = require("../utilities/filehelper");
 
-router.get('/reports',upload.single("file"), reports)
-
+// router.get('/reports',upload.single("file"), reports)
+router.post('/reports_predict',upload.single("file"), reports)
 module.exports = router;
