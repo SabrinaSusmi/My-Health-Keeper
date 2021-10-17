e18b0cd18331491ec04ef1d0ac6c4a81c5833391
SabrinaSusmi <sabrina.su512@gmail.com>
2021-10-17 13:08:40 +0600

    reports controller updated

--- backend/controllers/reports.controller.js
+++ backend/controllers/reports.controller.js
@@ -1,8 +1,8 @@
 const { spawn } = require("child_process");
 const ReportModel = require("../models/reports.model");
 const reports = async (req, res) => {
-  const { s1, s2, s3, s4, s5 } = req.headers;
-  // console.log(s1)
+  //const { s1, s2, s3, s4, s5 } = req.headers;
+   console.log("hello", req.file)
   const image = req.file.path.split("\\");
   console.log(image[1]);
   const reportsave = new ReportModel({
@@ -14,22 +14,22 @@ const reports = async (req, res) => {
   reportsave
     .save()
     .then(() => {
-     await ReportModel.find({filePath: req.file.path}).then((ans)=>{res.send({image:ans})}).catch((e)=>{console.log(e)})
+      ReportModel.find({filePath: req.file.path}).then((ans)=>{res.send({image:ans})}).catch((e)=>{console.log(e)})
     })
     .catch((er) => {
       console.log(er);
     });
-  const imgPath = "../backend/ML/" + image[1];
-  console.log("imgPath ", imgPath);
-  const pyProg = spawn("python", ["../backend/ML/reports.py", imgPath]);
+  // const imgPath = "../backend/ML/" + image[1];
+  // console.log("imgPath ", imgPath);
+  // const pyProg = spawn("python", ["../backend/ML/reports.py", imgPath]);
 
-  pyProg.stdout.on("data", function (data) {
-    const ans = data.toString();
-    console.log(ans);
-  });
-  pyProg.on("close", function (code) {
-    console.log(code);
-  });
+  // pyProg.stdout.on("data", function (data) {
+  //   const ans = data.toString();
+  //   console.log(ans);
+  // });
+  // pyProg.on("close", function (code) {
+  //   console.log(code);
+  // });
 };
 
 const fileSizeFormatter = (bytes, decimal) => {
