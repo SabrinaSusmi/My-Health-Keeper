0f281f8df2c69e847352e878e957f0562bcdb63f
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-10-17 21:29:46 +0600

    reports almost done

--- frontend/src/components/body/reports/reportsMain.js
+++ frontend/src/components/body/reports/reportsMain.js
@@ -1,7 +1,14 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { useSelector, useDispatch } from "react-redux";
 import { Link, Redirect, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import "../../../static/Styling/reports.css"
+import axios from "axios";
+import { ShowHeader } from "../../header/Header";
+import { ShowFeatureButtons } from "../../header/featureButton";
+import { Container, Row, Col } from "react-grid-system";
+import featureButton from "../../../static/Styling/featureButton.css";
+import { COLORS } from "../../themeColors";
+import DeleteIcon from "@material-ui/icons/Delete";
 import {
   showSuccessMsg,
   showErrMsg,
@@ -42,9 +49,9 @@ function ReportsMain() {
         const output = res.data.ans;
         // console.log(parseFloat(res.data.ans));
         if (res.data.ans == 0.0) {
-          setAns("NOT Pneumonia");
+          setAns("You do not have Pneumonia!");
         } else {
-          setAns("Pneumonia");
+          setAns("You have Pneumonia.");
         }
       });
   };
@@ -92,49 +99,112 @@ function ReportsMain() {
   //       });
 
   return (
-    <div
-      className="report_image_div"
-      onMouseOver={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div>
-        {err && showErrMsg(err)}
-        {success && showSuccessMsg(success)}
-        {loading && <h3>Loading.....</h3>}
-      </div>
-      {avatar ? (
-        <img
-          style={{ maxWidth: 1000 }}
-          className="report_image"
-          src={`http://localhost:5000/${avatar["image"]["filePath"]}`}
-          alt="img"
-        />
-      ) : (
-        ""
-      )}
-      <h6 align="center"> Change Profile Image</h6>
-      <input
-        type="file"
-        className="form-control"
-        name="file"
-        id="file_up"
-        onChange={handleSubmit}
-      />
-
-      <div className="add_btn_diet">
-        <Button
-          type="submit"
-          className="add_btn"
-          variant="contained"
-          onClick={() => {
-            handlePrediction(avatar["image"]["filePath"]);
+    <>
+      <div
+        className="reminder"
+        style={{
+          backgroundImage: "url(/img/reports1.jpg)",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          height: "50vh",
+          backgroundPosition: "center",
+          backgroundAttachment: "fixed",
+         
+        }}
+      >
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            margin: 0,
+            maxWidth: '100%',
+            padding: 0,
+            marginRight: 0,
           }}
-          color="white"
         >
-          <font className="add_btn_diet_font"> predict</font>
-        </Button>
+          <div style={{ backgroundColor: "black", color: "black" }}>
+            {ShowHeader(COLORS.diseasePrediction)}
+          </div>
+          <pre></pre>
+          <pre></pre> <pre></pre> <pre></pre> <pre></pre>
+          <pre></pre>
+          <Row className="body_feature_row">
+            <Col style={{ display: "flex", flexDirection: "column" }}>
+              {" "}
+              &nbsp;
+              <div className="reports_header_content">
+                <pre></pre> <pre></pre> <pre></pre>
+              </div>
+              <div className="description_section_reports">
+                <p>Upload an X-ray image and find out if you have pneumonia.</p>
+              </div>
+              <div className="reports_upload_body">
+              <div
+                  className="report_image_div"
+                  onMouseOver={() => setIsHovered(true)}
+                  onMouseLeave={() => setIsHovered(false)}
+                >
+                  <div>
+                    {err && showErrMsg(err)}
+                    {success && showSuccessMsg(success)}
+                    {loading && <h3>Loading.....</h3>}
+                  </div>
+                  {avatar ? (
+                    <img
+                      style={{ maxWidth: 1000 }}
+                      className="report_image"
+                      src={`http://localhost:5000/${avatar["image"]["filePath"]}`}
+                      alt="img"
+                    />
+                  ) : (
+                    ""
+                  )}
+                  <h6 align="center"> Upload a chest x-ray Image</h6>
+                  <input
+                    type="file"
+                    className="form-control"
+                    name="file"
+                    id="file_up"
+                    onChange={handleSubmit}
+                  />
+
+                  <div className="reports_predict_btn">
+                    <Button
+                      type="submit"
+                      className="add_btn"
+                      variant="contained"
+                      onClick={() => {
+                        handlePrediction(avatar["image"]["filePath"]);
+                      }}
+                      color="white"
+                    >
+                      <font className="add_btn_diet_font"> predict</font>
+                    </Button>
+                  </div>
+                </div>
+                
+              </div>
+              <div className="prediction_body">
+                {ans?( <div className="reports_prediction">{ans} <br></br> Our model is only 83% accurate. Please consult with a doctor to become sure.</div>):(' ')}
+               
+              </div>
+              <pre></pre>
+              <pre></pre>
+              <pre></pre>
+              <pre></pre>
+            </Col>
+            <Col
+              className="body_feature_column"
+              style={{ position: "fixed" }}
+              sm={2}
+            >
+              {ShowFeatureButtons()}
+            </Col>
+          </Row>
+        </Container>
       </div>
-    </div>
+    </>
+    
   );
 }
 
