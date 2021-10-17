03eaac64c73c87654b5bfd040ebb7d5335eaab7a
SabrinaSusmi <sabrina.su512@gmail.com>
2021-10-17 12:43:21 +0600

    reports router updated

--- backend/routers/reports.router.js
+++ backend/routers/reports.router.js
@@ -4,5 +4,6 @@ const { reports } = require("../controllers/reports.controller");
 const { upload } = require("../utilities/filehelper");
 
 router.get('/reports',upload.single("file"), reports)
+router.post('/reports_predict',upload.single("file"), reports)
 
 module.exports = router;
