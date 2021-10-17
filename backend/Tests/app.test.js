e5492d4a75ebd427112816fa3b6c72d53406d010
Siana Rizwan <sianarizwan@gmail.com>
2021-10-17 19:15:55 +0600

    3 basic testing successfull

--- backend/Tests/app.test.js
+++ backend/Tests/app.test.js
@@ -1,12 +1,16 @@
+
 const request = require("supertest");
 const app = require("../app");
 const chai = require("chai");
+const express = require('express');
+const should = require('should');
 var expect = chai.expect
+
 
 
 describe("Jest Status", () => {
   it('Testing to see if Jest works', () => {
-      expect(1).toBe(1)
+      expect(1).to.equal(1)
     })
   
   });
@@ -16,4 +20,28 @@ describe("App Test", () => {
     });
   });
 
+  
 
+  describe("Redirect Test", () => {
+  it('should handle redirects', function (done) {
+    const app = express();
+
+    app.get('/login', function (req, res) {
+      res.end('Login');
+    });
+
+    app.get('/', function (req, res) {
+      res.redirect('/login');
+    });
+
+    request(app)
+      .get('/')
+      .redirects(1)
+      .end(function (err, res) {
+        should.exist(res);
+        res.status.should.be.equal(200);
+        res.text.should.be.equal('Login');
+        done();
+      });
+  });
+});
