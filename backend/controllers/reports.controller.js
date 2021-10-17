02883142c53dfe7c80d246d9e0c84e79ed2d867b
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-10-17 15:29:33 +0600

    prediction is added

--- backend/controllers/reports.controller.js
+++ backend/controllers/reports.controller.js
@@ -4,7 +4,7 @@ const reports = async (req, res) => {
   //const { s1, s2, s3, s4, s5 } = req.headers;
   //  console.log("hello", req.file)
   const image = req.file.path.split("\\");
-  console.log(image[1]);
+  // console.log(image[1]);
   const reportsave = new ReportModel({
     fileName: req.file.originalname,
     filePath: req.file.path,
@@ -16,30 +16,37 @@ const reports = async (req, res) => {
     .then(() => {
       ReportModel.find({ filePath: req.file.path })
         .then((ans) => {
-          console.log("hhhh ", ans[0]);
+          // console.log("hhhh ", ans[0]);
 
           res.send({ image: ans[0] });
         })
         .catch((e) => {
           console.log(e);
-        }); 
+        });
     })
     .catch((er) => {
       console.log(er);
     });
-  const imgPath = "../backend/uploads/" + image[1];
-  console.log("imgPath ", imgPath);
-  const pyProg = spawn("python", ["../backend/ML/reports.py", imgPath]);
-
-  pyProg.stdout.on("data", function (data) {
-    const ans = data.toString();
-    console.log(ans);
-  });
-  pyProg.on("close", function (code) {
-    console.log(code);
-  });
 };
 
+const prediction = async (req, res) => {
+  try {
+    const img=req.headers['img']
+const image=img.split('\\')
+    const imgPath = "../backend/uploads/" + image[1];
+    console.log("imgPath ", imgPath);
+    const pyProg = spawn("python", ["../backend/ML/reports.py", imgPath]);
+
+    pyProg.stdout.on("data", function (data) {
+      const ans = data.toString();
+      console.log(ans);
+      res.send({ ans: ans });
+    });
+    pyProg.on("close", function (code) {
+      console.log(code);
+    });
+  } catch {}
+};
 const fileSizeFormatter = (bytes, decimal) => {
   if (bytes === 0) {
     return "0 Bytes";
@@ -51,4 +58,4 @@ const fileSizeFormatter = (bytes, decimal) => {
     parseFloat((bytes / Math.pow(1000, index)).toFixed(dm)) + " " + sizes[index]
   );
 };
-module.exports = { reports };
+module.exports = { reports, prediction };
