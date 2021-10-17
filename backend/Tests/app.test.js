ca6865ce8f70a820eff6905b0d03a0a53a6ee203
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-10-17 12:22:35 +0600

    Update app.test.js

--- backend/Tests/app.test.js
+++ backend/Tests/app.test.js
@@ -1,5 +1,7 @@
 const request = require("supertest");
 const app = require("../app");
+const chai = require("chai");
+var expect = chai.expect
 
 describe("test1", () => {
   it("server is connected", (done) => {
