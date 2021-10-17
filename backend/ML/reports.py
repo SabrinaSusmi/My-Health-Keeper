f485fb504a5e7abc741ae54996b0b56b46092c17
Siana Rizwan <sianarizwan@gmail.com>
2021-10-17 09:18:35 +0600

    backend for reports updated

--- backend/ML/reports.py
+++ backend/ML/reports.py
@@ -1,12 +1,12 @@
 import os
 import h5py
-import tensorflow as tf
 # import keras as keras
 import tensorflow.keras 
 from tensorflow.keras.models import load_model
 import cv2 as cv2
 import matplotlib
 from numpy import loadtxt
+import sys
 # from keras.models import load_model
 
 
@@ -16,10 +16,13 @@ from numpy import loadtxt
 # summarize model.
 
 
+
+
 model = load_model('./xray_model.h5')
 model.summary()
-load_image = cv2.imread(r'D:\\3-1\\Design Project\\My-Health-Keeper\\backend\\ML\\NORMAL2-IM-0086-0001.jpeg')
+load_image = cv2.imread(str(sys.argv[1]))
 img_new = cv2.resize(load_image, (180,180))
+
 new = img_new.reshape(1,img_new.shape[0], img_new.shape[1],3)
 result = model.predict(new, steps=10)
-print(result)
+print(result[0][0])
