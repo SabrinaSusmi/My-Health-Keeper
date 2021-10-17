251d59600b976aeda98ccaf3eaabbac6afb418c3
SabrinaSusmi <sabrina.su512@gmail.com>
2021-10-17 16:05:33 +0600

    router for reports updated

--- backend/routers/reports.router.js
+++ backend/routers/reports.router.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const auth=require("../middleware/auth")
-const { reports } = require("../controllers/reports.controller");
+const { reports,prediction } = require("../controllers/reports.controller");
 const { upload } = require("../utilities/filehelper");
 
-router.get('/reports',upload.single("file"), reports)
-router.post('/reports_predict',auth,upload.single("file"), reports)
-
+router.get('/reports', prediction)
+router.post('/reports_predict',upload.single("file"), reports)
 module.exports = router;
