bd527e8692365f091c0ac42ef8625601d77b1217
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-10-17 23:21:18 +0600

    bug solved

--- backend/controllers/menstrualCycle.controllers/cycleTrackerControllers.js
+++ backend/controllers/menstrualCycle.controllers/cycleTrackerControllers.js
@@ -54,15 +54,18 @@ const cycleTrackerControllers = {
 
     await Cycle.find({ user: user })
       .then((ans) => {
-        const currentDate = new Date();
-        const lastEndDate = ans[0]["endDate"];
-        console.log(currentDate.getTime(), "         ", lastEndDate.getTime());
-
-        // console.log(typeof(date2),'   ',typeof(currentDate))
-        let diff = Math.abs(currentDate - lastEndDate);
-        let cycleLength = diff / (1000 * 60 * 60 * 24);
-        // console.log(Math.floor(cycleLength))
-        res.send({CycleLength:Math.floor(cycleLength)});
+        if(ans.length>0){
+          const currentDate = new Date();
+          const lastEndDate = ans[0]["endDate"];
+          console.log(currentDate.getTime(), "         ", lastEndDate.getTime());
+  
+          // console.log(typeof(date2),'   ',typeof(currentDate))
+          let diff = Math.abs(currentDate - lastEndDate);
+          let cycleLength = diff / (1000 * 60 * 60 * 24);
+          // console.log(Math.floor(cycleLength))
+          res.send({CycleLength:Math.floor(cycleLength)});
+        }
+      
       })
       .catch((err) => {
         console.log(err);
@@ -76,9 +79,12 @@ const cycleTrackerControllers = {
 
     await Cycle.find({ user: user })
       .then((ans) => {
-        const cycleLength = ans[0]["cycleLength"];
-        console.log('cycleLength ',cycleLength)
-        res.send({cycleLength:cycleLength});
+        if(ans.length>0){
+          const cycleLength = ans[0]["cycleLength"];
+          console.log('cycleLength ',cycleLength)
+          res.send({cycleLength:cycleLength});
+        }
+    
       })
       .catch((err) => {
         console.log(err);
