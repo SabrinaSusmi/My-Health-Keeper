088033df08bf63ac3009c3b5c9819df9f69069e8
Siana Rizwan <sianarizwan@gmail.com>
2021-10-17 18:42:00 +0600

    2 basic testing done

--- backend/Tests/app.test.js
+++ backend/Tests/app.test.js
@@ -3,8 +3,17 @@ const app = require("../app");
 const chai = require("chai");
 var expect = chai.expect
 
-describe("test1", () => {
+
+describe("Jest Status", () => {
+  it('Testing to see if Jest works', () => {
+      expect(1).toBe(1)
+    })
+  
+  });
+describe("App Test", () => {
     it("server is connected", (done) => {
       request(app).get("/").expect("App running", done).expect(200);
     });
   });
+
+
