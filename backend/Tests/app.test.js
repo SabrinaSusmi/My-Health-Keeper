22e4404d1cc27e3da356b871026ca18c7efb8264
Siana Rizwan <sianarizwan@gmail.com>
2021-10-18 09:15:02 +0600

    Basic testing

--- backend/Tests/app.test.js
+++ backend/Tests/app.test.js
@@ -1,47 +1,10 @@
-
 const request = require("supertest");
 const app = require("../app");
 const chai = require("chai");
-const express = require('express');
-const should = require('should');
 var expect = chai.expect
 
-
-
-describe("Jest Status", () => {
-  it('Testing to see if Jest works', () => {
-      expect(1).to.equal(1)
-    })
-  
-  });
-describe("App Test", () => {
-    it("server is connected", (done) => {
-      request(app).get("/").expect("App running", done).expect(200);
-    });
-  });
-
-  
-
-  describe("Redirect Test", () => {
-  it('should handle redirects', function (done) {
-    const app = express();
-
-    app.get('/login', function (req, res) {
-      res.end('Login');
-    });
-
-    app.get('/', function (req, res) {
-      res.redirect('/login');
-    });
-
-    request(app)
-      .get('/')
-      .redirects(1)
-      .end(function (err, res) {
-        should.exist(res);
-        res.status.should.be.equal(200);
-        res.text.should.be.equal('Login');
-        done();
-      });
+describe("test1", () => {
+  it("server is connected", (done) => {
+    request(app).get("/").expect("App running", done);
   });
 });
