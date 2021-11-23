c3eb9570a5839c4d98420fdd4ca8e4eb8d6d25d0
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-11-23 12:08:04 +0600

    Update MenstrualHistoryModal.js

--- frontend/src/components/body/MenstrualCycle/MenstrualHistoryModal.js
+++ frontend/src/components/body/MenstrualCycle/MenstrualHistoryModal.js
@@ -3,53 +3,93 @@ import { Line } from "react-chartjs-2";
 import "react-responsive-modal/styles.css";
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "@material-ui/core";
+import { Button, Select, IconButton,Grid } from "@material-ui/core";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 
 const HistoryModal = ({ showWeightModal, setShowWeightModal }) => {
   const token = useSelector((state) => state.token);
   const [chartData, setChartData] = useState({});
+  const [year, setYear] = useState("0");
+  const [consumedCaloriesData, setYearlyMenstrualCycleLengthData] = useState(
+    {}
+  );
 
-  const chart = () => {
-    let day_array = [];
-    let date_array = [];
-    axios
-      .get("http://localhost:5000/getChart/Weight", {
-        headers: { Authorization: token },
-      }) //get info and input date from db
+  useEffect(() => {
+    menstrualCycleLengthChart();
+  }, []);
+
+  const menstrualCycleLengthChart = () => {
+    let cycleLengthArray = [];
+    let monthArray = [];
+    setYearlyMenstrualCycleLengthData({
+      labels: monthArray,
+      datasets: [
+        {
+          data: cycleLengthArray,
+          label: "Number Of Days",
+          fill: false,
+          lineTension: 0.5,
+          backgroundColor: "#6aa84f",
+          borderColor: "#6aa84f",
+          borderWidth: 2,
+        },
+      ],
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    let cycleLengthArray = [];
+    // let req_cal_data = [];
+    let monthArray = [];
+    e.preventDefault();
+    console.log(year);
+    await axios
+      .get(
+        "http://localhost:5000/user/getGraphData",
+
+        {
+          headers: { Authorization: token, year: year },
+        }
+      )
       .then((res) => {
-
-        res.data.infoData.forEach(element => {
-            day_array.push(element);
+        console.log(year);
+        res.data.cycleLengthArray.forEach((element) => {
+          cycleLengthArray.push(element);
         });
-        res.data.dates.forEach(element=> {
-            date_array.push(element);
+        cycleLengthArray.push(22);
+        cycleLengthArray.push(32);
+        // res.data.req_cal.forEach((element) => {
+        //   req_cal_data.push(element);
+        // });
+        res.data.monthArray.forEach((element) => {
+          monthArray.push(element);
         });
-        console.log(date_array);
+        //  setToalCal(res.data.totalCal);
+        console.log(cycleLengthArray);
 
-        setChartData({
-          labels: date_array,
+        setYearlyMenstrualCycleLengthData({
+          labels: monthArray,
           datasets: [
             {
-              data: day_array,
-              label: "Weight (Last 7 days)",
+              data: cycleLengthArray,
+              label: "Number Of Days",
               fill: false,
               lineTension: 0.5,
-              backgroundColor: "rgba(75,192,192,1)",
-              borderColor: "rgba(0,0,0,1)",
+              backgroundColor: "#f4bf20",
+              borderColor: "#f4bf20",
               borderWidth: 2,
+              pointRadius: 5,
             },
           ],
         });
       })
       .catch((err) => {
-        console.log(err);
+        // err.response.data.msg &&
+        // setItem({ ...item, err: err.response.data.msg, success: "" });
       });
   };
-  useEffect(() => {
-    chart();
-  }, []);
 
   const closeWeightModal = () => {
     setShowWeightModal(false);
@@ -59,6 +99,7 @@ const HistoryModal = ({ showWeightModal, setShowWeightModal }) => {
     <>
       <Modal
         size="lg"
+       
         aria-labelledby="contained-modal-title-vcenter"
         centered
         show={showWeightModal}
@@ -66,23 +107,55 @@ const HistoryModal = ({ showWeightModal, setShowWeightModal }) => {
       >
         <Modal.Header>
           <Modal.Title>
-            <h4>Weight</h4>
+            <h4>Visualize Your Menstruation Data </h4>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h2>Graph</h2>
-
+          <Select
+            // className={classes.formControl}
+            type="text"
+            id="selectedMonth"
+            name="selectedMonth"
+            className="menstrual_year_progress_select"
+            // defaultValue={currentMonth}
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+            // padding="10px"
+            label="selectedMonth"
+          >
+            <option disabled value="0">
+              Select Year
+            </option>
+            <option value={"2021"}>2021</option>
+            <option value={"2020"}>2020</option>
+            <option value={"2019"}>2019</option>
+            <option value={"2018"}>2018</option>
+            <option value={"2017"}>2017</option>
+            <option value={"2016"}>2016</option>
+            <option value={"2015"}>2015</option>
+            <option value={"2014"}>2014</option>
+            <option value={"2013"}>2013</option>
+            <option value={"2012"}>2012</option>
+            <option value={"2011"}>2011</option>
+          </Select>
+          <IconButton onClick={(e) => handleSubmit(e)} style={{ padding: 0 }}>
+            <VisibilityIcon />
+          </IconButton>
+          <Grid container >
+            <Grid item xs={5}>
+            <br></br>
+Cycle Length
           <Line
-            data={chartData}
+            data={consumedCaloriesData}
             options={{
               responsive: true,
-              title: { text: "Weight Graph", display: true },
+              title: { text: "Cycle Length", display: true },
               scales: {
                 yAxes: [
                   {
                     ticks: {
                       autoSkip: true,
-                      maxTicksLimit: 10,
+                      maxTicksLimit: 12,
                       beginAtZero: true,
                     },
                     gridLines: {
@@ -100,6 +173,46 @@ const HistoryModal = ({ showWeightModal, setShowWeightModal }) => {
               },
             }}
           />
+
+
+            </Grid>
+            <Grid item xs={5}>
+            <br></br>
+Cycle Length
+          <Line
+            data={consumedCaloriesData}
+            options={{
+              responsive: true,
+              title: { text: "Cycle Length", display: true },
+              scales: {
+                yAxes: [
+                  {
+                    ticks: {
+                      autoSkip: true,
+                      maxTicksLimit: 12,
+                      beginAtZero: true,
+                    },
+                    gridLines: {
+                      display: false,
+                    },
+                  },
+                ],
+                xAxes: [
+                  {
+                    gridLines: {
+                      display: false,
+                    },
+                  },
+                ],
+              },
+            }}
+          />
+
+
+            </Grid>
+            </Grid>
+
+
         </Modal.Body>
         <Modal.Footer>
           <Button
@@ -115,8 +228,5 @@ const HistoryModal = ({ showWeightModal, setShowWeightModal }) => {
     </>
   );
 };
-
-
-
 
 export default HistoryModal;
