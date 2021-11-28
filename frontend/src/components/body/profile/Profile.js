3a3463b4aa49920f1de25f7fb20fdcfcff355bb8
SabrinaSusmi <sabrina.su512@gmail.com>
2021-11-28 23:25:31 +0600

    profile edited

--- frontend/src/components/body/profile/Profile.js
+++ frontend/src/components/body/profile/Profile.js
@@ -17,15 +17,13 @@ import {
   Typography,
   CardContent,
   TextField,
-  
 } from "@material-ui/core";
 import { ShowHeader } from "../../header/Header";
 import { ShowFeatureButtons } from "../../header/featureButton";
 // import { makeStyles } from "@material-ui/core/styles";
 import { Container, Row, Col } from "react-grid-system";
-import featureButton from '../../../static/Styling/featureButton.css'
+import featureButton from "../../../static/Styling/featureButton.css";
 import { COLORS } from "../../themeColors";
-
 
 const initialState = {
   name: "",
@@ -62,20 +60,20 @@ function Profile() {
         setAvatar(res.data);
       });
   };
-  const showPaymentStatus=async(state)=>{
-await axios
-.get("http://localhost:5000/user/user_payment_status", {
-  headers: { Authorization: token },
-})
-.then((res) => {
-  console.log(res.data)
-setIsPaid(res.data)
-});
-  }
+  const showPaymentStatus = async (state) => {
+    await axios
+      .get("http://localhost:5000/user/user_payment_status", {
+        headers: { Authorization: token },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setIsPaid(res.data);
+      });
+  };
 
   useEffect(async () => {
     showAvatar(state);
-    showPaymentStatus(state)
+    showPaymentStatus(state);
   }, []);
 
   const changeAvatar = async (e) => {
@@ -196,8 +194,8 @@ setIsPaid(res.data)
       paddingTop: "8%",
       paddingLeft: "33%",
     },
-    emailField:{
-      fontWeight:'bold'
+    emailField: {
+      fontWeight: "bold",
     },
   }));
 
@@ -206,208 +204,235 @@ setIsPaid(res.data)
   return (
     <>
       <div
-        class="bg_image"
-        style={{
-          backgroundImage: "url(/img/profile.jpg)",
-          backgroundAttachment: "fixed",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          height: "50vh",
-        }}
+      // class="bg_image"
+      // style={{
+      //   backgroundImage: "url(/img/profile.jpg)",
+      //   backgroundAttachment: "fixed",
+      //   backgroundSize: "cover",
+      //   backgroundRepeat: "no-repeat",
+      //   height: "50vh",
+      // }}
       >
-      <Container className='body_container' style={{display: 'flex', flexDirection: 'column' ,margin:0,maxWidth:1900,padding:0,marginRight:0}} >
-    <div >{ShowHeader(COLORS.profileBackground)}</div>
-    
-    
-     <pre></pre> <pre></pre> <pre></pre>
-    <pre></pre>
-    <Row className='body_feature_row' >
-     
-      <Col style={{ display: 'flex', flexDirection: 'column'}}>
-      <div className="sp_header_content">
-                 {err && showErrMsg(err)}
-          {success && showSuccessMsg(success)}
-          {loading && <h3>Loading.....</h3>}
-        </div>
-        <div className="card_body_profile " >
-          {
-            <Card className="profile_root" >
-              <div className="profile_div" >
-                <CardContent className="profile_content"  >
-                  <div
-                    className="profile_image_div"
-                    onMouseOver={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
-                  >
-                    <img
-                      className="profile_image"
-                      src={`http://localhost:5000/${avatar.filePath}`}
-                      alt="img"
-                    />
-                    <div></div>
-                    {isHovered && (
-                      <>
-                        <h6 align="center"> Change Profile Image</h6>
-                        <input
-                          type="file"
-                          className="form-control"
-                          name="file"
-                          id="file_up"
-                          onChange={changeAvatar}
-                        />
-                      </>
-                    )}
-                  </div>
-                  {loading && <h3>Loading.....</h3>}
-                  <div className="profile_input">
-                    <label className="profile_label" htmlFor="name">
-                      Name
-                    </label>{" "}
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <TextField
-                      type="text"
-                      name="name"
-                      id="name"
-                      defaultValue={user.name}
-                      placeholder="Your name"
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className="profile_input">
-                    <label className="profile_label" htmlFor="email">
-                      Email
-                    </label>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <TextField
-className={classes.emailField}                      type="email"
-                      name="email"
-                      id="email"
-                      defaultValue={user.email}
-                      placeholder="Your email address"
-                      disabled
-                      
-                    />
-                  </div>
-                  <div className="profile_input">
-                    <label className="profile_label" htmlFor="phone">
-                      Phone
-                    </label>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <TextField
-                      type="text"
-                      name="phone"
-                      id="phone"
-                      defaultValue={user.phone}
-                      placeholder="Contact Number"
-                      onChange={handleChange}
-                    />
-                  </div>
-                </CardContent>
+        <Container
+          className="body_container"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            margin: 0,
+            maxWidth: 1900,
+            padding: 0,
+            marginRight: 0,
+          }}
+        >
+          <div>{ShowHeader(COLORS.profileBackground)}</div>
+          <pre></pre> <pre></pre> <pre></pre>
+          <pre></pre>
+          <Row className="body_feature_row">
+            <Col style={{ display: "flex", flexDirection: "column" }}>
+              <div>
+                {err && showErrMsg(err)}
+                {success && showSuccessMsg(success)}
+                {loading && <h3>Loading.....</h3>}
               </div>
-            </Card>
-          }
-          {
-            <Card className="profile_root" >
-              <div className="profile_div" >
-                <CardContent className="profile_content" >
-                  <Grid className="grid_profile">
-                    <Card className="card_grid_profile">
-                      <CardContent className="card_content_grid_profile" >
-                        <h5 align="center">
-                          <b>UPGRADE TO PREMIUM</b>
-                        </h5>
-                        Get SMS alert for Daily Medicines for just 100bdt per
-                        month
-                        <pre></pre> <pre></pre>
-                        <h6>
-                          {" "}
-                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                          <b>SMS Alert Activated&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b>{isPaid? "🟢":"🔴"}
-                        </h6>
-                        
-                        <div className="profile_upgrade_btn">
-                        &nbsp;&nbsp; &nbsp;  <Button align='center' variant="text" onClick={onHandleCheckout}>
-                            UPGRADE
-                          </Button>
+              <div className="card_body_profile ">
+                {
+                  <Card className="profile_root">
+                    <div className="profile_div">
+                      <CardContent className="profile_content">
+                        <div
+                          className="profile_image_div"
+                          onMouseOver={() => setIsHovered(true)}
+                          onMouseLeave={() => setIsHovered(false)}
+                        >
+                          <img
+                            className="profile_image"
+                            src={`http://localhost:5000/${avatar.filePath}`}
+                            alt="img"
+                          />
+                          <div></div>
+                          {isHovered && (
+                            <>
+                              <h6 align="center"> Change Profile Image</h6>
+                              <input
+                                type="file"
+                                className="form-control"
+                                name="file"
+                                id="file_up"
+                                onChange={changeAvatar}
+                              />
+                            </>
+                          )}
+                        </div>
+                        {loading && <h3>Loading.....</h3>}
+                        <div className="profile_input">
+                          <label className="profile_label" htmlFor="name">
+                            Name
+                          </label>{" "}
+                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                          <TextField
+                            type="text"
+                            name="name"
+                            id="name"
+                            defaultValue={user.name}
+                            placeholder="Your name"
+                            onChange={handleChange}
+                          />
+                        </div>
+                        <div className="profile_input">
+                          <label className="profile_label" htmlFor="email">
+                            Email
+                          </label>
+                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                          <TextField
+                            className={classes.emailField}
+                            type="email"
+                            name="email"
+                            id="email"
+                            defaultValue={user.email}
+                            placeholder="Your email address"
+                            disabled
+                          />
+                        </div>
+                        <div className="profile_input">
+                          <label className="profile_label" htmlFor="phone">
+                            Phone
+                          </label>
+                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                          <TextField
+                            type="text"
+                            name="phone"
+                            id="phone"
+                            defaultValue={user.phone}
+                            placeholder="Contact Number"
+                            onChange={handleChange}
+                          />
                         </div>
                       </CardContent>
-                    </Card>
+                    </div>
+                  </Card>
+                }
+                {
+                  <Card className="profile_root">
+                    <div className="profile_div">
+                      <CardContent className="profile_content">
+                        <Grid className="grid_profile">
+                          <Card className="card_grid_profile">
+                            <CardContent className="card_content_grid_profile">
+                              <h5 align="center">
+                                <b>UPGRADE TO PREMIUM</b>
+                              </h5>
+                              Get SMS alert for Daily Medicines for just 100bdt
+                              per month
+                              <pre></pre> <pre></pre>
+                              <h6>
+                                {" "}
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <b>
+                                  SMS Alert
+                                  Activated&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                </b>
+                                {isPaid ? "🟢" : "🔴"}
+                              </h6>
+                              <div className="profile_upgrade_btn">
+                                &nbsp;&nbsp; &nbsp;{" "}
+                                <Button
+                                  align="center"
+                                  variant="text"
+                                  onClick={onHandleCheckout}
+                                >
+                                  UPGRADE
+                                </Button>
+                              </div>
+                            </CardContent>
+                          </Card>
 
-                    <Card className="card_grid_profile">
-                      <CardContent className="card_content_grid_profile">
-                        <h7><b>PERSONAL INFORMATION</b></h7>
-                        <pre></pre>
-                        <div className="grid_profile_input">
-              <label className="grid_profile_label">Gender  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-              <Select
-                className={classes.formControl}
-                type="text"
-                id="gender"
-                name="gender"
-                onChange={handleChange}
-                defaultValue={user.gender}
-                value={gender}
-                padding="10px"
-                label="Gender"
-              >
-                <option value={"Male"}>Male</option>
-                <option value={"Female"}>Female</option>
-                <option value={"Others"}>Others</option>
-              </Select>
-            </div>
+                          <Card className="card_grid_profile">
+                            <CardContent className="card_content_grid_profile">
+                              <h7>
+                                <b>PERSONAL INFORMATION</b>
+                              </h7>
+                              <pre></pre>
+                              <div className="grid_profile_input">
+                                <label className="grid_profile_label">
+                                  Gender &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                </label>
+                                <Select
+                                  className={classes.formControl}
+                                  type="text"
+                                  id="gender"
+                                  name="gender"
+                                  onChange={handleChange}
+                                  defaultValue={user.gender}
+                                  value={gender}
+                                  padding="10px"
+                                  label="Gender"
+                                >
+                                  <option value={"Male"}>Male</option>
+                                  <option value={"Female"}>Female</option>
+                                  <option value={"Others"}>Others</option>
+                                </Select>
+                              </div>
 
-            <div className="grid_profile_input">
-                    <label className="grid_profile_label" htmlFor="name">
-                    Blood Group
-                    </label>{" "}
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <TextField
-                      type="text"
-                      name="bloodGrp"
-                      id="bloodGrp"
-                      defaultValue={user.bloodGrp}
-                      placeholder="Your Blood Group"
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className="grid_profile_input">
-                    <label className="grid_profile_label" htmlFor="email">
-                    Height
-                    </label>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <TextField
-                      type="text"
-                      name="height"
-                      id="height"
-                      defaultValue={user.height}
-                      placeholder="Your Height"
-                      onChange={handleChange}
-                    />
-                  </div>
-                
+                              <div className="grid_profile_input">
+                                <label
+                                  className="grid_profile_label"
+                                  htmlFor="name"
+                                >
+                                  Blood Group
+                                </label>{" "}
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <TextField
+                                  type="text"
+                                  name="bloodGrp"
+                                  id="bloodGrp"
+                                  defaultValue={user.bloodGrp}
+                                  placeholder="Your Blood Group"
+                                  onChange={handleChange}
+                                />
+                              </div>
+                              <div className="grid_profile_input">
+                                <label
+                                  className="grid_profile_label"
+                                  htmlFor="email"
+                                >
+                                  Height
+                                </label>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <TextField
+                                  type="text"
+                                  name="height"
+                                  id="height"
+                                  defaultValue={user.height}
+                                  placeholder="Your Height"
+                                  onChange={handleChange}
+                                />
+                              </div>
+                            </CardContent>
+                          </Card>
+                        </Grid>
                       </CardContent>
-                    </Card>
-                  </Grid>
-                </CardContent>
+                    </div>
+                  </Card>
+                }
               </div>
-            </Card>
-          }
-        </div>
-        <div className="profile_save_btn">
-        <Button varient="text" onClick={handleUpdate}>
-        &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;     save
-              </Button>
-        </div>
-        </Col>
-        <Col className='body_feature_column' style={{ position:'fixed' }} sm={2}>
-      <pre></pre>
-    <pre></pre>
-        {ShowFeatureButtons()}</Col>
-    </Row>
-  </Container>
+              <div className="profile_save_btn">
+                <Button varient="text" onClick={handleUpdate}>
+                  &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; save
+                </Button>
+              </div>
+            </Col>
+            <Col
+              className="body_feature_column"
+              style={{ position: "fixed" }}
+              sm={2}
+            >
+              <pre></pre>
+              <pre></pre>
+              {ShowFeatureButtons()}
+            </Col>
+          </Row>
+        </Container>
       </div>
-
-   
     </>
   );
 }
